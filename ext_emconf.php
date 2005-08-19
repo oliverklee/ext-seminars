8f522a9f3f99c7b297a1104cf4d4d7b8a5f1fe85
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-08-19 17:40:42 +0000

    New MD5 values.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@1407 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "seminars"
 # 
-# Auto generated 30-07-2005 18:19
+# Auto generated 18-08-2005 23:11
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'private' => 0,
 	'download_password' => '',
 	'version' => '0.3.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:36:{s:30:"class.tx_seminars_dbplugin.php";s:4:"dbb9";s:34:"class.tx_seminars_registration.php";s:4:"839b";s:41:"class.tx_seminars_registrationmanager.php";s:4:"971d";s:29:"class.tx_seminars_seminar.php";s:4:"8d31";s:36:"class.tx_seminars_templatehelper.php";s:4:"1bfe";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4757";s:14:"ext_tables.php";s:4:"65b9";s:14:"ext_tables.sql";s:4:"3f46";s:24:"ext_typoscript_setup.txt";s:4:"6c9e";s:19:"flexform_pi1_ds.xml";s:4:"735e";s:32:"icon_tx_seminars_attendances.gif";s:4:"475a";s:31:"icon_tx_seminars_organizers.gif";s:4:"475a";s:36:"icon_tx_seminars_payment_methods.gif";s:4:"475a";s:29:"icon_tx_seminars_seminars.gif";s:4:"475a";s:26:"icon_tx_seminars_sites.gif";s:4:"475a";s:29:"icon_tx_seminars_speakers.gif";s:4:"475a";s:13:"locallang.php";s:4:"a22f";s:16:"locallang_db.php";s:4:"d146";s:7:"tca.php";s:4:"df58";s:8:"todo.txt";s:4:"38d8";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"579b";s:14:"mod1/index.php";s:4:"b250";s:18:"mod1/locallang.php";s:4:"ef46";s:22:"mod1/locallang_mod.php";s:4:"cf13";s:19:"mod1/moduleicon.gif";s:4:"8074";s:29:"pi1/class.tx_seminars_pi1.php";s:4:"4755";s:17:"pi1/locallang.php";s:4:"6e85";s:21:"pi1/seminars_pi1.tmpl";s:4:"58f6";s:14:"doc/manual.sxw";s:4:"6abc";s:15:"doc/CVS/Entries";s:4:"cf84";s:18:"doc/CVS/Repository";s:4:"d876";s:12:"doc/CVS/Root";s:4:"788f";s:40:"static/tx_srfeuserregister_pi1_tmpl.diff";s:4:"18ad";s:40:"static/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"92ae";}',
+	'_md5_values_when_last_written' => 'a:33:{s:30:"class.tx_seminars_dbplugin.php";s:4:"dbb9";s:34:"class.tx_seminars_registration.php";s:4:"6819";s:41:"class.tx_seminars_registrationmanager.php";s:4:"7a73";s:29:"class.tx_seminars_seminar.php";s:4:"1a36";s:36:"class.tx_seminars_templatehelper.php";s:4:"68e2";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4757";s:14:"ext_tables.php";s:4:"82b1";s:14:"ext_tables.sql";s:4:"b31a";s:24:"ext_typoscript_setup.txt";s:4:"f3b9";s:19:"flexform_pi1_ds.xml";s:4:"72bf";s:32:"icon_tx_seminars_attendances.gif";s:4:"475a";s:31:"icon_tx_seminars_organizers.gif";s:4:"475a";s:36:"icon_tx_seminars_payment_methods.gif";s:4:"475a";s:29:"icon_tx_seminars_seminars.gif";s:4:"475a";s:26:"icon_tx_seminars_sites.gif";s:4:"475a";s:29:"icon_tx_seminars_speakers.gif";s:4:"475a";s:13:"locallang.php";s:4:"6701";s:16:"locallang_db.php";s:4:"4e6e";s:7:"tca.php";s:4:"9840";s:8:"todo.txt";s:4:"f002";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"579b";s:14:"mod1/index.php";s:4:"dfb0";s:18:"mod1/locallang.php";s:4:"ef46";s:22:"mod1/locallang_mod.php";s:4:"cf13";s:19:"mod1/moduleicon.gif";s:4:"8074";s:29:"pi1/class.tx_seminars_pi1.php";s:4:"e45a";s:17:"pi1/locallang.php";s:4:"9f13";s:20:"pi1/seminars_pi1.css";s:4:"b99b";s:21:"pi1/seminars_pi1.tmpl";s:4:"3922";s:14:"doc/manual.sxw";s:4:"5fd3";s:40:"static/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"92ae";}',
 );
 
 ?>
