d1ac809d683d05fa18afbfdcc1a977ab53075fdc
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-09 12:19:24 +0000

    New version.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@1504 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "seminars"
 # 
-# Auto generated 09-10-2005 14:15
+# Auto generated 09-10-2005 14:18
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.3.2',	// Don't modify this! Managed automatically during upload to repository.
+	'version' => '0.4.0',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:35:{s:30:"class.tx_seminars_dbplugin.php";s:4:"712c";s:34:"class.tx_seminars_registration.php";s:4:"751f";s:41:"class.tx_seminars_registrationmanager.php";s:4:"e83a";s:40:"class.tx_seminars_salutationswitcher.php";s:4:"5fb1";s:29:"class.tx_seminars_seminar.php";s:4:"c91e";s:36:"class.tx_seminars_templatehelper.php";s:4:"d2e5";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4757";s:14:"ext_tables.php";s:4:"06bb";s:14:"ext_tables.sql";s:4:"dbb6";s:24:"ext_typoscript_setup.txt";s:4:"28df";s:19:"flexform_pi1_ds.xml";s:4:"58f3";s:32:"icon_tx_seminars_attendances.gif";s:4:"475a";s:31:"icon_tx_seminars_organizers.gif";s:4:"475a";s:36:"icon_tx_seminars_payment_methods.gif";s:4:"475a";s:29:"icon_tx_seminars_seminars.gif";s:4:"475a";s:26:"icon_tx_seminars_sites.gif";s:4:"475a";s:29:"icon_tx_seminars_speakers.gif";s:4:"475a";s:13:"locallang.php";s:4:"3409";s:16:"locallang_db.php";s:4:"d25d";s:13:"seminars.tmpl";s:4:"3285";s:7:"tca.php";s:4:"8bdd";s:8:"todo.txt";s:4:"26f2";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"579b";s:14:"mod1/index.php";s:4:"b915";s:18:"mod1/locallang.php";s:4:"70ba";s:22:"mod1/locallang_mod.php";s:4:"c6a5";s:19:"mod1/moduleicon.gif";s:4:"8074";s:29:"pi1/class.tx_seminars_pi1.php";s:4:"16bd";s:17:"pi1/locallang.php";s:4:"d460";s:20:"pi1/seminars_pi1.css";s:4:"3546";s:21:"pi1/seminars_pi1.tmpl";s:4:"12a6";s:14:"doc/manual.sxw";s:4:"4423";s:40:"static/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"6310";}',
 );
 
