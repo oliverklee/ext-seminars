f153436c2d2ff1b32551e9821402bc455d04b1eb
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-29 20:46:59 +0000

    Add setence that had been left out in the formal localization.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@1508 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/locallang.php
+++ pi1/locallang.php
@@ -105,7 +105,7 @@ $LOCAL_LANG = Array (
 		'label_onlineRegistration' => 'Online anmelden',
 		'label_back' => 'Zur Übersicht',
 		'label_registrationForm' => 'Anmeldedaten',
-		'label_thankyou' => 'Vielen Dank für Ihre Anmeldung! Wir freuen uns auf Sie und wünschen Ihnen viel Erfolg bei der Veranstaltung.',
+		'label_thankyou' => 'Vielen Dank für Ihre Anmeldung! Eine E-Mail mit der Anmeldebestätigung ist unterwegs. Wir freuen uns auf Sie und wünschen Ihnen viel Erfolg bei der Veranstaltung.',
 		'label_thankyou_informal' => 'Vielen Dank für deine Anmeldung! Eine E-Mail mit der Anmeldebestätigung ist unterwegs. Wir freuen uns auf dich und wünschen dir viel Erfolg bei der Veranstaltung.',
 
 		'message_wrongSeminarNumber' => 'Eine Veranstaltung mit dieser Nummer existiert nicht. Wenn Sie diese Fehlermeldung durch einen Link auf unserer Seite bekommen haben, benachrichtigen Sie bitte den Webmaster. Vielen Dank!',
