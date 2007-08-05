fdc0884e0f955dbe30d4fc136e397572ac1366f6
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-08-05 17:16:27 +0000

    Patched trunk for bugs: - [#1042] Fix: Another PHP error on the registration page - [#1039] Fix: Error on register attendances
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@6142 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_seminars_registration.php
+++ class.tx_seminars_registration.php
@@ -32,6 +32,8 @@
  * @author		Oliver Klee <typo3-coding@oliverklee.de>
  */
 
+require_once(PATH_t3lib.'class.t3lib_befunc.php');
+require_once(PATH_t3lib.'class.t3lib_refindex.php');
 require_once(t3lib_extMgm::extPath('seminars').'class.tx_seminars_objectfromdb.php');
 
 class tx_seminars_registration extends tx_seminars_objectfromdb {
