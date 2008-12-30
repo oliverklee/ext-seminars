d72ce61779704b07fe7fe063b0aefea20a3ff315
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-30 18:36:15 +0000

    [Bug 2491] Add a class tx_seminars_mod2_BackEndModule, r=niels
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@15365 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ mod2/class.tx_seminars_mod2_BackEndModule.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Oliver Klee (typo3-coding@oliverklee.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(PATH_t3lib . 'class.t3lib_scbase.php');
+
+/**
+ * Module 'tx_seminars_mod2_BackEndModule' for the 'seminars' extension.
+ *
+ * This class is the base class for a back-end module.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class tx_seminars_mod2_BackEndModule extends t3lib_SCbase {
+	/**
+	 * @var array data of the current BE page
+	 */
+	private $pageData = array();
+
+	/**
+	 * Frees as much memory used by this object as possible.
+	 */
+	public function __destruct() {
+		unset($this->doc, $this->extObj, $this->pageData);
+	}
+
+	/**
+	 * Returns the data of the current BE page.
+	 *
+	 * @return array the data of the current BE page, may be emtpy
+	 */
+	public function getPageData() {
+		return $this->pageData;
+	}
+
+	/**
+	 * Sets the data for the current BE page.
+	 *
+	 * @param array page data, may be empty
+	 */
+	public function setPageData(array $pageData) {
+		$this->pageData = $pageData;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/mod2/class.tx_seminars_mod2_BackEndModule.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/mod2/class.tx_seminars_mod2_BackEndModule.php']);
+}
+?>
---
+++ tests/tx_seminars_mod2_BackEndModule_testcase.php
@@ -0,0 +1,71 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Oliver Klee (typo3-coding@oliverklee.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+/**
+ * Testcase for the tx_seminars_mod2_BackEndModule class in the 'seminars' extensions.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class tx_seminars_mod2_BackEndModule_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_seminars_mod2_BackEndModule
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->fixture = new tx_seminars_mod2_BackEndModule();
+	}
+
+	public function tearDown() {
+		$this->fixture->__destruct();
+
+		unset($this->fixture);
+	}
+
+
+	////////////////////////////////////////////////
+	// Tests for getting and setting the page data
+	////////////////////////////////////////////////
+
+	public function testGetPageDataInitiallyReturnsEmptyArray() {
+		$this->assertEquals(
+			array(),
+			$this->fixture->getPageData()
+		);
+	}
+
+	public function testGetPageDataReturnsCompleteDataSetViaSetPageData() {
+		$this->fixture->setPageData(array('foo' => 'bar'));
+
+		$this->assertEquals(
+			array('foo' => 'bar'),
+			$this->fixture->getPageData()
+		);
+	}
+}
+?>
