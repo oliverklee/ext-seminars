0fb2c9dd8c6b844d087df53fe744a2e8da0d64ec
bernd.schoenbach <bernd.schoenbach@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-31 17:25:58 +0000

    [Bug 2429] On the registration page, list the requirements for a registration if they are not fulfilled, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@15393 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/class.tx_seminars_pi1_frontEndRequirementsList.php
@@ -0,0 +1,137 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Bernd Schönbach <bernd@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_seminars_pi1_frontEndRequirementsList' for the 'seminars' extension.
+ *
+ * This class is a view which creates the requirements lists for the front end.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Bernd Schönbach <bernd@oliverklee.de>
+ */
+class tx_seminars_pi1_frontEndRequirementsList extends tx_seminars_pi1_frontEndView {
+	/**
+	 * @var string the path to this script relative to the extension dir
+	 */
+	public $scriptRelPath = 'pi1/class.tx_seminars_pi1_frontEndRequirementsList.php';
+
+	/**
+	 * @var tx_seminars_seminar the event to build the requirements list for
+	 */
+	private $event = null;
+
+	/**
+	 * @var boolean whether to limit the requirements to the events the user
+	 *              still needs to register
+	 */
+	private $limitRequirementsToMissing = false;
+
+	/**
+	 * The destructor.
+	 */
+	public function __destruct() {
+		unset($this->event);
+		parent::__destruct();
+	}
+
+	/**
+	 * Sets the event to which this view relates.
+	 *
+	 * @param tx_seminars_seminar the event to build the requirements list for
+	 */
+	public function setEvent(tx_seminars_seminar $event) {
+		$this->event = $event;
+	}
+
+	/**
+	 * Limits the requirements list to the requirements the user still needs to
+	 * register to.
+	 */
+	public function limitToMissingRegistrations() {
+		if (!$this->isLoggedIn()) {
+			throw new Exception(
+				'No FE user is currently logged in. Please call ' .
+				'this function only when a FE user is logged in.'
+			);
+		}
+		$this->setMarker(
+			'label_requirements',
+			$this->translate('label_registration_requirements')
+		);
+		$this->limitRequirementsToMissing = true;
+	}
+
+	/**
+	 * Creates the list of required events.
+	 *
+	 * @return string HTML code of the list, will not be empty
+	 */
+	public function render() {
+		if (!$this->event) {
+			throw new Exception(
+				'No event was set, please set an event before calling render.'
+			);
+		}
+
+		$output = '';
+
+		foreach ($this->getRequirements() as $requirement) {
+			$this->setMarker(
+				'requirement_title',
+				$requirement->getLinkedFieldValue($this, 'title')
+			);
+			$output .= $this->getSubpart('SINGLE_REQUIREMENT');
+		}
+		$this->setSubpart('SINGLE_REQUIREMENT', $output);
+
+		return $this->getSubpart('FIELD_WRAPPER_REQUIREMENTS');
+	}
+
+	/**
+	 * Returns the requirements which should be displayed.
+	 *
+	 * @return tx_seminars_seminarbag the requirements still to be displayed,
+	 *                                may be empty
+	 */
+	private function getRequirements() {
+		if ($this->limitRequirementsToMissing) {
+			$registrationManager
+				= t3lib_div::makeInstance('tx_seminars_registrationmanager');
+			$result = $registrationManager->getMissingRequiredTopics(
+				$this->event
+			);
+			$registrationManager->__destruct();
+		} else {
+			$result = $this->event->getRequirements();
+		}
+
+		return $result;
+	}
+}
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/pi1/class.tx_seminars_pi1_frontEndRequirementsList.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/pi1/class.tx_seminars_pi1_frontEndRequirementsList.php']);
+}
+?>
---
+++ tests/tx_seminars_pi1_frontEndRequirementsList_testcase.php
@@ -0,0 +1,247 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2008 Bernd Schönbach <bernd@oliverklee.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the 'pi1_frontEndRequirementsList' class in the 'seminars' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Bernd Schönbach <bernd@oliverklee.de>
+ */
+class tx_seminars_pi1_frontEndRequirementsList_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_seminars_pi1_frontEndRequirementsList
+	 */
+	private $fixture;
+
+	/**
+	 * @var integer the UID of a seminar to which the plugin relates
+	 */
+	private $seminarUid;
+
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	public function setUp() {
+		$this->testingFramework	= new tx_oelib_testingFramework('tx_seminars');
+		$this->testingFramework->createFakeFrontEnd();
+
+		$this->seminarUid = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'pid' => $this->systemFolderPid,
+				'title' => 'Test event',
+			)
+		);
+
+		$this->fixture = new tx_seminars_pi1_frontEndRequirementsList(
+			array('templateFile' => 'EXT:seminars/pi1/seminars_pi1.tmpl'),
+			$GLOBALS['TSFE']->cObj
+		);
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework);
+	}
+
+
+	//////////////////////////////////
+	// Tests for the render function
+	//////////////////////////////////
+
+	public function testRenderWithoutSetSeminarThrowsException() {
+		$this->setExpectedException(
+			'Exception',
+			'No event was set, please set an event before calling render'
+		);
+
+		$this->fixture->render();
+	}
+
+	public function testRenderShowsTitleOfOneRequirement() {
+		$this->testingFramework->changeRecord(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			array('object_type' => SEMINARS_RECORD_TYPE_TOPIC)
+		);
+		$requiredEvent = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_foo',
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent, 'requirements'
+		);
+		$this->fixture->setEvent(new tx_seminars_seminar($this->seminarUid));
+
+		$this->assertContains(
+			'required_foo',
+			$this->fixture->render()
+		);
+	}
+
+	public function testRenderLinksOneRequirementToTheSingleView() {
+		$this->fixture->setConfigurationValue(
+			'detailPID',
+			$this->testingFramework->createFrontEndPage()
+		);
+		$this->testingFramework->changeRecord(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			array('object_type' => SEMINARS_RECORD_TYPE_TOPIC)
+		);
+		$requiredEvent = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_foo',
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent, 'requirements'
+		);
+		$this->fixture->setEvent(new tx_seminars_seminar($this->seminarUid));
+
+		$this->assertRegExp(
+			'/<a href=.*' . $requiredEvent . '.*>required_foo<\/a>/',
+			$this->fixture->render()
+		);
+	}
+
+	public function testRenderShowsTitleOfTwoRequirements() {
+		$this->testingFramework->changeRecord(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			array('object_type' => SEMINARS_RECORD_TYPE_TOPIC)
+		);
+		$requiredEvent1 = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_foo',
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent1, 'requirements'
+		);
+		$requiredEvent2 = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_bar',
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent2, 'requirements'
+		);
+		$this->fixture->setEvent(new tx_seminars_seminar($this->seminarUid));
+
+		$this->assertRegExp(
+			'/required_foo.*required_bar/s',
+			$this->fixture->render()
+		);
+	}
+
+
+	///////////////////////////////////
+	// Tests for limiting the results
+	///////////////////////////////////
+
+	public function testtLimitToMissingRegistrationsWithNoLoggedInFeUserThrowsException() {
+		$this->setExpectedException(
+			'Exception',
+			'No fe-user is currently logged in. Please call ' .
+				'this function only when a fe-user is logged in.'
+		);
+
+		$this->fixture->limitToMissingRegistrations();
+	}
+
+	public function testLimitToMissingRegistrationsLimitsOutputToMissingRegistrationsOnly() {
+		$userUid = $this->testingFramework->createAndLoginFrontEndUser();
+		$this->testingFramework->changeRecord(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			array('object_type' => SEMINARS_RECORD_TYPE_TOPIC)
+		);
+		$requiredEvent1 = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_foo',
+			)
+		);
+		$this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_DATE,
+				'topic' => $requiredEvent1,
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent1, 'requirements'
+		);
+		$requiredEvent2 = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_TOPIC,
+				'title' => 'required_bar',
+			)
+		);
+		$requiredDate2 = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SEMINARS,
+			array(
+				'object_type' => SEMINARS_RECORD_TYPE_DATE,
+				'topic' => $requiredEvent2,
+			)
+		);
+		$this->testingFramework->createRelationAndUpdateCounter(
+			SEMINARS_TABLE_SEMINARS, $this->seminarUid,
+			$requiredEvent2, 'requirements'
+		);
+		$this->testingFramework->createRecord(
+			SEMINARS_TABLE_ATTENDANCES,
+			array('seminar' => $requiredDate2, 'user' => $userUid)
+		);
+		$this->fixture->setEvent(new tx_seminars_seminar($this->seminarUid));
+		$this->fixture->limitToMissingRegistrations();
+
+		$this->assertNotContains(
+			'required_bar',
+			$this->fixture->render()
+		);
+	}
+}
+?>
