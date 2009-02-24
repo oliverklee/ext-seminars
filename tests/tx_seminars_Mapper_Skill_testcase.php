2001f9252b050586911afa6a96edf01ab526f99c
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-24 22:05:44 +0000

    [Bug 2752] Feature: Add skill model and mapper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@17184 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/tx_seminars_Mapper_Skill_testcase.php
@@ -0,0 +1,89 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the 'skill mapper' class in the 'seminars' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_Mapper_Skill_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	/**
+	 * @var tx_seminars_Mapper_Skill
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->testingFramework = new tx_oelib_testingFramework('tx_seminars');
+
+		$this->fixture = new tx_seminars_Mapper_Skill();
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework);
+	}
+
+
+	//////////////////////////
+	// Tests concerning find
+	//////////////////////////
+
+	/**
+	 * @test
+	 */
+	public function findWithUidOfExistingRecordReturnsSkillInstance() {
+		$uid = $this->testingFramework->createRecord(SEMINARS_TABLE_SKILLS);
+
+		$this->assertTrue(
+			$this->fixture->find($uid) instanceof tx_seminars_Model_Skill
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function findWithUidOfExistingRecordReturnsRecordAsModel() {
+		$uid = $this->testingFramework->createRecord(
+			SEMINARS_TABLE_SKILLS, array('title' => 'Superhero')
+		);
+
+		$this->assertEquals(
+			'Superhero',
+			$this->fixture->find($uid)->getTitle()
+		);
+	}
+}
+?>
