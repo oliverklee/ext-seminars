e58ff36f9adbbc63ae5b4e9ac434583ec4b09402
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-27 21:55:56 +0000

    [Bug 2778] Feature: Add payment method model and mapper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@17326 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/tx_seminars_Model_PaymentMethod_testcase.php
@@ -0,0 +1,149 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the 'payment method model' class in the 'seminars' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_Model_PaymentMethod_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_seminars_Model_PaymentMethod
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->fixture = new tx_seminars_Model_PaymentMethod();
+	}
+
+	public function tearDown() {
+		$this->fixture->__destruct();
+		unset($this->fixture);
+	}
+
+
+	/**
+	 * @test
+	 */
+	public function setTitleWithEmptyTitleThrowsException() {
+		$this->setExpectedException(
+			'Exception', 'The parameter $title must not be empty.'
+		);
+
+		$this->fixture->setTitle('');
+	}
+
+	/**
+	 * @test
+	 */
+	public function setTitleSetsTitle() {
+		$this->fixture->setTitle('Cash');
+
+		$this->assertEquals(
+			'Cash',
+			$this->fixture->getTitle()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getTitleWithNonEmptyTitleReturnsTitle() {
+		$this->fixture->setData(array('title' => 'Cash'));
+
+		$this->assertEquals(
+			'Cash',
+			$this->fixture->getTitle()
+		);
+	}
+
+
+	/////////////////////////////////////
+	// Tests regarding the description.
+	/////////////////////////////////////
+
+	/**
+	 * @test
+	 */
+	public function getDescriptionWithoutDescriptionReturnsAnEmptyString() {
+		$this->fixture->setData(array());
+
+		$this->assertEquals(
+			'',
+			$this->fixture->getDescription()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getDescriptionWithDescriptionReturnsDescription() {
+		$this->fixture->setData(array('description' => 'Just plain cash, baby!'));
+
+		$this->assertEquals(
+			'Just plain cash, baby!',
+			$this->fixture->getDescription()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function setDescriptionSetsDescription() {
+		$this->fixture->setDescription('Just plain cash, baby!');
+
+		$this->assertEquals(
+			'Just plain cash, baby!',
+			$this->fixture->getDescription()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function hasDescriptionWithoutDescriptionReturnsFalse() {
+		$this->fixture->setData(array());
+
+		$this->assertFalse(
+			$this->fixture->hasDescription()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function hasDescriptionWithDescriptionReturnsTrue() {
+		$this->fixture->setDescription('Just plain cash, baby!');
+
+		$this->assertTrue(
+			$this->fixture->hasDescription()
+		);
+	}
+}
+?>
