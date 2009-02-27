ad287fd5000e069c358883a54f4f028059fc0d67
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-27 21:10:45 +0000

    [Bug 2776] Feature: Add event type model and mapper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@17323 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Model/class.tx_seminars_Model_EventType.php
@@ -0,0 +1,62 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_seminars_Model_EventType' for the 'seminars' extension.
+ *
+ * This class represents a event type.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_Model_EventType extends tx_oelib_Model {
+	/**
+	 * Returns our title.
+	 *
+	 * @return string our title, will not be empty
+	 */
+	public function getTitle() {
+		return $this->getAsString('title');
+	}
+
+	/**
+	 * Sets our title.
+	 *
+	 * @param string our title to set, must not be empty
+	 */
+	public function setTitle($title) {
+		if ($title == '') {
+			throw new Exception('The parameter $title must not be empty.');
+		}
+
+		$this->setAsString('title', $title);
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/Model/class.tx_seminars_Model_EventType.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/seminars/Model/class.tx_seminars_Model_EventType.php']);
+}
+?>
