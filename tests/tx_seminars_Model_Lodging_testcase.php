92fb6874f0785fd6e1e70dc3b54f6d2dab39cd63
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-28 12:40:38 +0000

    [Bug 2782] Feature: Add lodging model and mapper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@17336 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/tx_seminars_Model_Lodging_testcase.php
@@ -0,0 +1,86 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Testcase for the 'lodging model' class in the 'seminars' extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_Model_Lodging_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_seminars_Model_Lodging
+	 */
+	private $fixture;
+
+	public function setUp() {
+		$this->fixture = new tx_seminars_Model_Lodging();
+	}
+
+	public function tearDown() {
+		$this->fixture->__destruct();
+		unset($this->fixture);
+	}
+
+
+	/**
+	 * @test
+	 */
+	public function setTitleWithEmptyTitleThrowsException() {
+		$this->setExpectedException(
+			'Exception', 'The parameter $title must not be empty.'
+		);
+
+		$this->fixture->setTitle('');
+	}
+
+	/**
+	 * @test
+	 */
+	public function setTitleSetsTitle() {
+		$this->fixture->setTitle('Shack');
+
+		$this->assertEquals(
+			'Shack',
+			$this->fixture->getTitle()
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getTitleWithNonEmptyTitleReturnsTitle() {
+		$this->fixture->setData(array('title' => 'Shack'));
+
+		$this->assertEquals(
+			'Shack',
+			$this->fixture->getTitle()
+		);
+	}
+}
+?>
