c422238d23dafc6d6d31c7734882c3bcbcce3429
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-23 10:55:25 +0000

    [Bug 840] Change: Rename and move the flexforms file
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@18176 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -291,7 +291,7 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:seminars/flexform_pi1_ds.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:seminars/pi1/flexforms_pi1.xml');
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Seminars');
 
