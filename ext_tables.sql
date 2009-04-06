a0530e855e18f00d40962e57c1f9a8d118aa98ad
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-06 09:51:34 +0000

    [Bug 2622] Fix: Error when trying to create price with access using IRRE
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@18737 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -741,7 +741,7 @@ CREATE TABLE tx_seminars_prices (
 	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
 	starttime int(11) unsigned DEFAULT '0' NOT NULL,
 	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-	fe_group int(11) unsigned DEFAULT '0' NOT NULL,
+	fe_group int(11) DEFAULT '0' NOT NULL,
 	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	seminar int(11) unsigned DEFAULT '0' NOT NULL,
 	title tinytext,
