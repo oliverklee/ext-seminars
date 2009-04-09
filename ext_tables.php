6bd428357318b178be4d91289eecc9b9e9e51c47
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-09 09:21:44 +0000

    [Bug 2910] Change: Move the flexforms XML file to Configuration/FlexForms
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@18878 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -291,7 +291,10 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:seminars/pi1/flexforms_pi1.xml');
+t3lib_extMgm::addPiFlexFormValue(
+	$_EXTKEY . '_pi1',
+	'FILE:EXT:seminars/Configuration/FlexForms/flexforms_pi1.xml'
+);
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/Settings', 'Seminars');
 
