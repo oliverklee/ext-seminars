304dc9341f66db91b1e38c3b62998a6f73a95a36
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-30 18:44:32 +0000

    [Bug 3213] Crash when saving an event in the FE editor, r=niels
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@22938 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,6 +4,7 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 include_once(t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_seminars_flexForms.php');
+include_once(t3lib_extMgm::extPath($_EXTKEY) . 'tx_seminars_modifiedSystemTables.php');
 
 t3lib_extMgm::addLLrefForTCAdescr(
 	'tx_seminars_seminars',
@@ -307,58 +308,6 @@ t3lib_extMgm::addPlugin(
 		t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif',
 	),
 	'list_type'
-);
-
-t3lib_div::loadTCA('fe_groups');
-t3lib_extMgm::addTCAcolumns(
-	'fe_groups',
-	array(
-		'tx_seminars_publish_events' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_publish_events',
-			'config' => array(
-				'type' => 'radio',
-				'default' => '0',
-				'items' => array(
-					array('LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_publish_events.I.0', '0'),
-					array('LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_publish_events.I.1', '1'),
-					array('LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_publish_events.I.2', '2'),
-				),
-			),
-		),
-		'tx_seminars_auxiliary_records_pid' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_auxiliary_records_pid',
-			'config' => array(
-				'type' => 'group',
-				'internal_type' => 'db',
-				'allowed' => 'pages',
-				'size' => '1',
-				'minitems' => '0',
-				'maxitems' => '1',
-			),
-		),
-		'tx_seminars_reviewer' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:seminars/locallang_db.xml:fe_groups.tx_seminars_reviewer',
-			'config' => array(
-				'type' => 'group',
-				'internal_type' => 'db',
-				'allowed' => 'be_users',
-				'size' => 1,
-				'minitems' => 0,
-				'maxitems' => 1,
-			),
-		),
-	),
-	1
-);
-
-t3lib_extMgm::addToAllTCAtypes(
-	'fe_groups',
-	'--div--;LLL:EXT:seminars/locallang_db.xml:fe_groups.tab_event_management,' .
-		'tx_seminars_publish_events;;;;1-1-1,tx_seminars_auxiliary_records_pid,'.
-		'tx_seminars_reviewer'
 );
 
 if (TYPO3_MODE == 'BE') {
