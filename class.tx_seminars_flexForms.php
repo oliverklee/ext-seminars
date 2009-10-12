b85c377811c6e5e43b19ce4cbadde56c3cc1e277
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-12 13:58:35 +0000

    [Bug 3440] Crash in the flexforms: Class not found, r=bernd
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@25296 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_seminars_flexForms.php
+++ class.tx_seminars_flexForms.php
@@ -22,6 +22,8 @@
 * This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
 require_once(t3lib_extMgm::extPath('seminars') . 'lib/tx_seminars_constants.php');
 
 require_once(t3lib_extMgm::extPath('lang') . 'lang.php');
