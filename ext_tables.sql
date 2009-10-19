b794944f68a0a6b102e4adb776f7cd7d7b96f06b
bernd.schoenbach <bernd.schoenbach@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-19 09:43:22 +0000

    [Bug 3279] Drop the picture from the speakers SQL and TCA,r=oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/branches/version_0_8_x@25557 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -299,7 +299,6 @@ CREATE TABLE tx_seminars_speakers (
 	homepage tinytext,
 	description text,
 	skills int(11) unsigned DEFAULT '0' NOT NULL,
-	picture blob,
 	notes text,
 	address text,
 	phone_work tinytext,
