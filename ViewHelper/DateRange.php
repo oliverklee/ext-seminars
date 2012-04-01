2aedb957489f1f0075e9248082b061715d771a6b
nielspardon <nielspardon@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-01 17:02:41 +0000

    [Bug 4419] Feature: Add a view helper for rendering the date of an event
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@60234 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ViewHelper/DateRange.php
@@ -0,0 +1,211 @@
+<?php
+/***************************************************************
+ * Copyright notice
+*
+* (c) 2012 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * This class represents a view helper for rendering date ranges.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_ViewHelper_DateRange {
+	/**
+	 * @var tx_oelib_Configuration
+	 */
+	protected $configuration = NULL;
+
+	/**
+	 * @var tx_oelib_Translator
+	 */
+	protected $translator = NULL;
+
+	/**
+	 * The constructor.
+	 */
+	public function __construct() {
+		$this->configuration = tx_oelib_ConfigurationRegistry::getInstance()->get('plugin.tx_seminars_seminars');
+		$this->translator = tx_oelib_TranslatorRegistry::getInstance()->get('seminars');
+	}
+
+	/**
+	 * Frees as much memory that has been used by this object as possible.
+	 */
+	public function __destruct() {
+		unset($this->configuration, $this->translator);
+	}
+
+	/**
+	 * Gets the date.
+	 * Returns a localized string "will be announced" if there's no date set.
+	 *
+	 * Returns just one day if the timespan takes place on only one day.
+	 * Returns a date range if the timespan takes several days.
+	 *
+	 * @param tx_seminars_Model_AbstractTimeSpan $timeSpan the timespan to get the date for
+	 * @param string $dash the character or HTML entity used to separate start date and end date
+	 *
+	 * @return string the timespan date
+	 */
+	public function render(tx_seminars_Model_AbstractTimeSpan $timeSpan, $dash = '&#8211;') {
+		if (!$timeSpan->hasBeginDate()) {
+			return $this->translator->translate('message_willBeAnnounced');
+		}
+
+		$beginDate = $timeSpan->getBeginDateAsUnixTimeStamp();
+		$endDate = $timeSpan->getEndDateAsUnixTimeStamp();
+
+		// Is the timespan open-ended or does it span one day only?
+		if (!$timeSpan->hasEndDate() || $this->isSameDay($beginDate, $endDate)) {
+			return $this->getAsDateFormatYmd($beginDate);
+		}
+
+		if ($this->configuration->getAsBoolean('abbreviateDateRanges')) {
+			$formattedBeginDate = $this->getAsAbbreviatedDateRange($beginDate, $endDate, $dash);
+		} else {
+			$formattedBeginDate = $this->getAsDateFormatYmd($beginDate);
+		}
+
+		return $formattedBeginDate . $dash . $this->getAsDateFormatYmd($endDate);
+	}
+
+	/**
+	 * Renders the UNIX timestamps in $beginDate and $endDate as an abbreviated date range.
+	 *
+	 * @param integer $beginDate
+	 * @param integer $endDate
+	 * @param string $dash
+	 *
+	 * @return string the abbreviated date range
+	 */
+	protected function getAsAbbreviatedDateRange($beginDate, $endDate, $dash = '&#8211;') {
+		// Are the years different? Then include the complete begin date.
+		if (!$this->isSameYear($beginDate, $endDate)) {
+			return $this->getAsDateFormatYmd($beginDate);
+		}
+
+		// Are the months different? Then include day and month.
+		if (!$this->isSameMonth($beginDate, $endDate)) {
+			return $this->getAsDateFormatMd($beginDate);
+		}
+
+		return $this->getAsDateFormatD($beginDate);
+	}
+
+	/**
+	 * Returns whether the UNIX timestamps in $beginDate and $endDate are on the same day.
+	 *
+	 * @param integer $beginDate
+	 * @param integer $endDate
+	 *
+	 * @return boolean TRUE if $beginDate and $endDate are on the same day, otherwise FALSE
+	 */
+	protected function isSameDay($beginDate, $endDate) {
+		return ($this->getAsDateFormatYmd($beginDate) === $this->getAsDateFormatYmd($endDate));
+	}
+
+	/**
+	 * Returns whether the UNIX timestamps in $beginDate and $endDate are in the same month.
+	 *
+	 * @param integer $beginDate
+	 * @param integer $endDate
+	 *
+	 * @return boolean TRUE if $beginDate and $endDate are in the same month, otherwise FALSE
+	 */
+	protected function isSameMonth($beginDate, $endDate) {
+		return ($this->getAsDateFormatM($beginDate) === $this->getAsDateFormatM($endDate));
+	}
+
+	/**
+	 * Returns whether the UNIX timestamps in $beginDate and $endDate are in the same year.
+	 *
+	 * @param integer $beginDate
+	 * @param integer $endDate
+	 *
+	 * @return boolean TRUE if $beginDate and $endDate are in the same year, otherwise FALSE
+	 */
+	protected function isSameYear($beginDate, $endDate) {
+		return ($this->getAsDateFormatY($beginDate) === $this->getAsDateFormatY($endDate));
+	}
+
+	/**
+	 * Renders a UNIX timestamp in the strftime format specified in plugin.tx_seminars_seminars.dateFormatYMD.
+	 *
+	 * @param integer $timestamp the UNIX timestamp to render
+	 *
+	 * @return string the UNIX timestamp rendered using the strftime format in plugin.tx_seminars_seminars.dateFormatYMD 
+	 */
+	protected function getAsDateFormatYmd($timestamp) {
+		return strftime($this->configuration->getAsString('dateFormatYMD'), $timestamp);
+	}
+
+	/**
+	 * Renders a UNIX timestamp in the strftime format specified in plugin.tx_seminars_seminars.dateFormatY.
+	 *
+	 * @param integer $timestamp the UNIX timestamp to render
+	 *
+	 * @return string the UNIX timestamp rendered using the strftime format in plugin.tx_seminars_seminars.dateFormatY
+	 */
+	protected function getAsDateFormatY($timestamp) {
+		return strftime($this->configuration->getAsString('dateFormatY'), $timestamp);
+	}
+
+	/**
+	 * Renders a UNIX timestamp in the strftime format specified in plugin.tx_seminars_seminars.dateFormatM.
+	 *
+	 * @param integer $timestamp the UNIX timestamp to render
+	 *
+	 * @return string the UNIX timestamp rendered using the strftime format in plugin.tx_seminars_seminars.dateFormatM
+	 */
+	protected function getAsDateFormatM($timestamp) {
+		return strftime($this->configuration->getAsString('dateFormatM'), $timestamp);
+	}
+
+	/**
+	 * Renders a UNIX timestamp in the strftime format specified in plugin.tx_seminars_seminars.dateFormatMD.
+	 *
+	 * @param integer $timestamp the UNIX timestamp to render
+	 *
+	 * @return string the UNIX timestamp rendered using the strftime format in plugin.tx_seminars_seminars.dateFormatMD
+	 */
+	protected function getAsDateFormatMd($timestamp) {
+		return strftime($this->configuration->getAsString('dateFormatMD'), $timestamp);
+	}
+
+	/**
+	 * Renders a UNIX timestamp in the strftime format specified in plugin.tx_seminars_seminars.dateFormatD.
+	 *
+	 * @param integer $timestamp the UNIX timestamp to render
+	 *
+	 * @return string the UNIX timestamp rendered using the strftime format in plugin.tx_seminars_seminars.dateFormatD
+	 */
+	protected function getAsDateFormatD($timestamp) {
+		return strftime($this->configuration->getAsString('dateFormatD'), $timestamp);
+	}
+}
+
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/seminars/ViewHelper/DateRange.php']) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/seminars/ViewHelper/DateRange.php']);
+}
+?>
---
+++ tests/ViewHelper/DateRangeTest.php
@@ -0,0 +1,232 @@
+<?php
+/***************************************************************
+ * Copyright notice
+*
+* (c) 2012 Niels Pardon (mail@niels-pardon.de)
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Testcase for the tx_seminars_ViewHelper_DateRange class.
+ *
+ * @package TYPO3
+ * @subpackage tx_seminars
+ *
+ * @author Niels Pardon <mail@niels-pardon.de>
+ */
+class tx_seminars_ViewHelper_DateRangeTest extends tx_phpunit_testcase {
+	/**
+	 * @var tx_seminars_ViewHelper_DateRange
+	 */
+	private $fixture;
+
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	/**
+	 * @var tx_oelib_Configuration
+	 */
+	private $configuration;
+
+	/**
+	 * @var tx_oelib_Translator
+	 */
+	private $translator;
+
+	/**
+	 * @var integer
+	 */
+	const BEGIN_DATE = 978303600;
+
+	/**
+	 * @var string
+	 */
+	const DATE_FORMAT_YMD = '%d.%m.%Y',
+		DATE_FORMAT_Y = '%Y',
+		DATE_FORMAT_M = '%m.',
+		DATE_FORMAT_MD = '%d.%m.',
+		DATE_FORMAT_D = '%d.';
+
+	public function setUp() {
+		$this->testingFramework	= new tx_oelib_testingFramework('tx_seminars');
+
+		$this->configuration = new tx_oelib_Configuration();
+		$this->configuration->setAsString('dateFormatYMD', self::DATE_FORMAT_YMD);
+		$this->configuration->setAsString('dateFormatY', self::DATE_FORMAT_Y);
+		$this->configuration->setAsString('dateFormatM', self::DATE_FORMAT_M);
+		$this->configuration->setAsString('dateFormatMD', self::DATE_FORMAT_MD);
+		$this->configuration->setAsString('dateFormatD', self::DATE_FORMAT_D);
+
+		tx_oelib_ConfigurationRegistry::getInstance()->set('plugin.tx_seminars_seminars', $this->configuration);
+
+		$this->translator = tx_oelib_TranslatorRegistry::getInstance()->get('seminars');
+
+		$this->fixture = new tx_seminars_ViewHelper_DateRange();
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework, $this->configuration, $this->translator);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithNoDatesReturnMessageWillBeAnnounced() {
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setData(array());
+
+		$this->assertSame(
+			$this->translator->translate('message_willBeAnnounced'),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginDateOnlyRendersOnlyBeginDate() {
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+
+		$this->assertSame(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithEqualBeginAndEndDateReturnsOnlyBeginDate() {
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$timeSpan->setEndDateAsUnixTimeStamp(self::BEGIN_DATE);
+
+		$this->assertSame(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnSameDayReturnsOnlyBeginDate() {
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$timeSpan->setEndDateAsUnixTimeStamp(self::BEGIN_DATE + 3600);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnDifferentDaysWithAbbreviateDateRangeFalseReturnsBothFullDatesSeparatedByDash() {
+		$this->configuration->setAsBoolean('abbreviateDateRanges', FALSE);
+
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$endDate = self::BEGIN_DATE + (2 * 86400);
+		$timeSpan->setEndDateAsUnixTimeStamp($endDate);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE) . '&#8211;' . strftime(self::DATE_FORMAT_YMD, $endDate),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnDifferentDaysButSameMonthWithAbbreviateDateRangeTrueReturnsOnlyDayOfBeginDateAndFullEndDateSeparatedByDash() {
+		$this->configuration->setAsBoolean('abbreviateDateRanges', TRUE);
+
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$endDate = self::BEGIN_DATE + (2 * 86400);
+		$timeSpan->setEndDateAsUnixTimeStamp($endDate);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_D, self::BEGIN_DATE) . '&#8211;' . strftime(self::DATE_FORMAT_YMD, $endDate),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnDifferentMonthWithAbbreviateDateRangeTrueReturnsDayAndMonthOfBeginDateAndFullEndDateSeparatedByDash() {
+		$this->configuration->setAsBoolean('abbreviateDateRanges', TRUE);
+
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$endDate = self::BEGIN_DATE + (32 * 86400);
+		$timeSpan->setEndDateAsUnixTimeStamp($endDate);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_MD, self::BEGIN_DATE) . '&#8211;' . strftime(self::DATE_FORMAT_YMD, $endDate),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnDifferentYearsWithAbbreviateDateRangeTrueReturnsFullBeginDateAndFullEndDateSeparatedByDash() {
+		$this->configuration->setAsBoolean('abbreviateDateRanges', TRUE);
+
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$endDate = self::BEGIN_DATE + (366 * 86400);
+		$timeSpan->setEndDateAsUnixTimeStamp($endDate);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE) . '&#8211;' . strftime(self::DATE_FORMAT_YMD, $endDate),
+			$this->fixture->render($timeSpan)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function renderWithTimeSpanWithBeginAndEndDateOnDifferentDaysWithAbbreviateDateRangeFalseReturnsBothFullDatesSeparatedBySpecifiedDash() {
+		$this->configuration->setAsBoolean('abbreviateDateRanges', FALSE);
+		$dash = '#DASH#';
+
+		$timeSpan = new tx_seminars_tests_fixtures_TestingTimeSpan();
+		$timeSpan->setBeginDateAsUnixTimeStamp(self::BEGIN_DATE);
+		$endDate = self::BEGIN_DATE + (2 * 86400);
+		$timeSpan->setEndDateAsUnixTimeStamp($endDate);
+
+		$this->assertEquals(
+			strftime(self::DATE_FORMAT_YMD, self::BEGIN_DATE) . $dash . strftime(self::DATE_FORMAT_YMD, $endDate),
+			$this->fixture->render($timeSpan, $dash)
+		);
+	}
+}
+?>
