31b7b2890c33c668f1537c78735f1a15018af1d7
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-21 11:24:31 +0000

    [Bug 4278] Update the copyright to 2012
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@63634 735d13b6-9817-0410-8766-e36946ffe9aa

--- cli/class.tx_seminars_cli_MailNotifier.php
+++ cli/class.tx_seminars_cli_MailNotifier.php
@@ -2,7 +2,7 @@
 /***************************************************************
 * Copyright notice
 *
-* (c) 2009-2011 Saskia Metzler <saskia@merlin.owl.de>
+* (c) 2009-2012 Saskia Metzler <saskia@merlin.owl.de>
 * All rights reserved
 *
 * This script is part of the TYPO3 project. The TYPO3 project is
--- tests/FrontEnd/EventEditorTest.php
+++ tests/FrontEnd/EventEditorTest.php
@@ -2,7 +2,7 @@
 /***************************************************************
 * Copyright notice
 *
-* (c) 2008-2011 Niels Pardon (mail@niels-pardon.de)
+* (c) 2008-2012 Niels Pardon (mail@niels-pardon.de)
 * All rights reserved
 *
 * This script is part of the TYPO3 project. The TYPO3 project is
