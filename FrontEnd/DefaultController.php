5ab401357864d82287d153ef22a7e7de2f24aa12
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-11 22:31:52 +0000

    [Bug 4512] HTML entities of field ###attached_files### are displayed only as plain text
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@81972 735d13b6-9817-0410-8766-e36946ffe9aa

--- FrontEnd/DefaultController.php
+++ FrontEnd/DefaultController.php
@@ -3398,7 +3398,7 @@ class tx_seminars_FrontEnd_DefaultController extends tx_oelib_templatehelper {
 
 		$attachedFiles = '';
 		foreach ($this->seminar->getAttachedFiles($this) as $attachedFile) {
-			$this->setMarker('attached_files_single_title', htmlspecialchars($attachedFile['name']));
+			$this->setMarker('attached_files_single_title', $attachedFile['name']);
 
 			$attachedFiles .= $this->getSubpart('ATTACHED_FILES_SINGLE_ITEM');
 		}
