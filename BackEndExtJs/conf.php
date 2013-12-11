11ad0b3e1c36b1415a65c62f6d2dec4a19fcd4c9
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-11 17:01:10 +0000

    [Bug 4818] Drop the ExtJS back-end module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminars/trunk@81945 735d13b6-9817-0410-8766-e36946ffe9aa

--- BackEndExtJs/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$MCONF['name']='web_txseminarsM1';
-
-$MCONF['access'] = 'user,group';
-$MCONF['script'] = '_DISPATCH';
-
-$MLANG['default']['tabs_images']['tab'] = '../BackEnd/moduleicon.gif';
-$MLANG['default']['ll_ref'] = 'LLL:EXT:seminars/BackEnd/locallang_mod.xml';
