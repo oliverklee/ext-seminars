f6ba0da52b88d9d92ff5321f4fb0c19e447ee0fa
Oliver Klee <typo3-coding@oliverklee.de>
2014-08-31 11:01:08 +0200

    [BUGFIX] #5085 Allow first_name and last_name in the registration form
    Change-Id: I8dcfd8be36b1e5aec70e4fb67a8f13d880fbb9f2
    Reviewed-on: http://review.typo3.org/32533
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- class.tx_seminars_configcheck.php
+++ class.tx_seminars_configcheck.php
@@ -1,26 +1,16 @@
 <?php
-/***************************************************************
-* Copyright notice
-*
-* (c) 2006-2014 Oliver Klee (typo3-coding@oliverklee.de)
-* All rights reserved
-*
-* This script is part of the TYPO3 project. The TYPO3 project is
-* free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* The GNU General Public License can be found at
-* http://www.gnu.org/copyleft/gpl.html.
-*
-* This script is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * This class checks the Seminar Manager configuration for basic sanity.
@@ -855,6 +845,8 @@ class tx_seminars_configcheck extends tx_oelib_configcheck {
 				'url',
 				'planned_disclaimer',
 				'footer',
+				'first_name',
+				'last_name',
 			)
 		);
 	}
@@ -1170,6 +1162,8 @@ class tx_seminars_configcheck extends tx_oelib_configcheck {
 				'company',
 				'gender',
 				'name',
+				'first_name',
+				'last_name',
 				'address',
 				'zip',
 				'city',
@@ -1194,7 +1188,7 @@ class tx_seminars_configcheck extends tx_oelib_configcheck {
 				'feuser_data',
 				'registration_data',
 				'terms',
-				'terms_2'
+				'terms_2',
 			)
 		);
 	}
