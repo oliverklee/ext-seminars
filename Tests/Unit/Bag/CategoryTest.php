9dd9975b9d1b03dd5aa26404e6b253b9d9d41c4e
Stefano Kowalke <blueduck@mailbox.org>
2015-12-23 10:58:05 +0100

    [CLEANUP] #5529 Rename tx_seminars_Bag_Category
    Change-Id: If6f15039c3fafab0cd6ef16929c36c340c385df0
    Reviewed-on: https://review.typo3.org/46181
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- Tests/Unit/Bag/CategoryTest.php
+++ Tests/Unit/Bag/CategoryTest.php
@@ -21,9 +21,9 @@
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  * @author Niels Pardon <mail@niels-pardon.de>
  */
-class tx_seminars_Bag_CategoryTest extends Tx_Phpunit_TestCase {
+class Tx_Seminars_Bag_CategoryTest extends Tx_Phpunit_TestCase {
 	/**
-	 * @var tx_seminars_Bag_Category
+	 * @var Tx_Seminars_Bag_Category
 	 */
 	private $fixture;
 
@@ -37,7 +37,7 @@ class tx_seminars_Bag_CategoryTest extends Tx_Phpunit_TestCase {
 
 		$this->testingFramework->createRecord('tx_seminars_categories');
 
-		$this->fixture = new tx_seminars_Bag_Category('is_dummy_record=1');
+		$this->fixture = new Tx_Seminars_Bag_Category('is_dummy_record=1');
 	}
 
 	protected function tearDown() {
