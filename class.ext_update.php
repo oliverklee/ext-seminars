09abd7afdc65705ae11af9019dd9f49cb0de2697
Oliver Klee <typo3-coding@oliverklee.de>
2016-07-18 22:37:22 +0200

    [CLEANUP] #5737 Remove the obsolete parts from the EM upgrade wizard
    Change-Id: I35d3d1764997fe02a178a9787cef62edc0df4a60
    Reviewed-on: https://review.typo3.org/49034
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- class.ext_update.php
+++ class.ext_update.php
@@ -11,14 +11,10 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /**
  * This class offers functions to update the database from one version to another.
  *
- *
- * @author Niels Pardon <mail@niels-pardon.de>
- * @author Bernd Schönbach <bernd@oliverklee.de>
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  */
 class ext_update
@@ -26,109 +22,20 @@ class ext_update
     /**
      * Returns the update module content.
      *
-     * @return string the update module content, will be empty if nothing was
-     *                updated
+     * @return string the update module content, will be empty if nothing was updated
      */
     public function main()
     {
-        $result = '';
-        try {
-            if ($this->needsToUpdateEventField('registrations')) {
-                $result .= $this->updateRegistrationsField();
-            }
-        } catch (Tx_Oelib_Exception_Database $exception) {
-            $result = '';
-        }
-
-        return $result;
+        return '';
     }
 
     /**
-     * Returns whether the update module may be accessed.
+     * Checks whether the update module may be accessed.
      *
-     * @return bool TRUE if the update module may be accessed, FALSE otherwise
+     * @return bool true if the update module may be accessed, false otherwise
      */
     public function access()
     {
-        if (!ExtensionManagementUtility::isLoaded('oelib')
-            || !ExtensionManagementUtility::isLoaded('seminars')
-        ) {
-            return false;
-        }
-        if (!Tx_Oelib_Db::existsTable('tx_seminars_seminars')
-            || !Tx_Oelib_Db::existsTable('tx_seminars_attendances')
-        ) {
-            return false;
-        }
-        if (!Tx_Oelib_Db::tableHasColumn(
-            'tx_seminars_seminars', 'registrations'
-        )) {
-            return false;
-        }
-
-        try {
-            $result = $this->needsToUpdateEventField('registrations');
-        } catch (Tx_Oelib_Exception_Database $exception) {
-            $result = false;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Checks whether there are events which need to be updated concerning a
-     * given DB field.
-     *
-     * @param string $fieldToUpdate the DB field to check for needing an update, must be 'registrations'
-     *
-     * @return bool TRUE if any rows need to be updated, FALSE otherwise
-     */
-    private function needsToUpdateEventField($fieldToUpdate)
-    {
-        switch ($fieldToUpdate) {
-            case 'registrations':
-                $whereClause = 'registrations = 0 AND EXISTS (' .
-                    'SELECT * FROM tx_seminars_attendances' .
-                    ' WHERE seminar = tx_seminars_seminars.uid ' .
-                    Tx_Oelib_Db::enableFields('tx_seminars_attendances') .
-                    ')';
-                break;
-            default:
-                throw new InvalidArgumentException(
-                    'needsToUpdateEventField was called with "' . $fieldToUpdate .
-                        '", but the allowed value is only "registrations"',
-                    1333291685
-                );
-                break;
-        }
-
-        return (Tx_Oelib_Db::count('tx_seminars_seminars', $whereClause) > 0);
-    }
-
-    /**
-     * Updates the "registrations" field of the event records.
-     *
-     * @return string information about the status of the update process, will not be empty
-     *
-     * @throws Tx_Oelib_Exception_Database
-     */
-    private function updateRegistrationsField()
-    {
-        $query = 'UPDATE tx_seminars_seminars SET registrations = ' .
-                '(SELECT COUNT(*) FROM tx_seminars_attendances' .
-                ' WHERE seminar = tx_seminars_seminars.uid ' .
-                Tx_Oelib_Db::enableFields('tx_seminars_attendances') . ')' .
-            ' WHERE registrations = 0 AND EXISTS (' .
-                'SELECT * FROM tx_seminars_attendances' .
-                ' WHERE seminar = tx_seminars_seminars.uid ' .
-                Tx_Oelib_Db::enableFields('tx_seminars_attendances') .
-            ')';
-        if (!$GLOBALS['TYPO3_DB']->sql_query($query)) {
-            throw new Tx_Oelib_Exception_Database();
-        }
-
-        return '<h2>Updating the events.registrations field.</h2>' .
-            '<p>Updating ' . $GLOBALS['TYPO3_DB']->sql_affected_rows() .
-            ' event records.</p>';
+        return false;
     }
 }
