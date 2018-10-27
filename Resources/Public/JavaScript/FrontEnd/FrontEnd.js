8a802e7afeb49927d352f6d976c01664f43912ec
Oliver Klee <typo3-coding@oliverklee.de>
2018-10-27 21:57:51 +0200

    [BUGFIX] Fix the inclusion of the JavaScript file
    Fix the async and defer attributes.
    
    Also remove the "part of the TYPO3 CMS" header from the JavaScript file.

--- Resources/Public/JavaScript/FrontEnd/FrontEnd.js
+++ Resources/Public/JavaScript/FrontEnd/FrontEnd.js
@@ -1,17 +1,4 @@
 /*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
-/*
  * This file provides some JavaScript functions for the seminars front-end
  * editor and the registration form.
  *
