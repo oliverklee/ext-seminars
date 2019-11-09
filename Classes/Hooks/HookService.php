9fe10a409b626cff3f8f79c521c95220a6faba83
Michael Kramer <m.kramer@mxp.de>
2019-11-09 17:08:59 +0100

    Drop old style class aliasing functionality

--- Classes/Hooks/HookService.php
+++ Classes/Hooks/HookService.php
@@ -114,8 +114,3 @@ class HookService
     }
 }
 
-if (\defined('TYPO3_MODE')
-    && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/seminars/Classes/Hooks/Hook.php'])
-) {
-    include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/seminars/Classes/Hooks/Hook.php']);
-}
