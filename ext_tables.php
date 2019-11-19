aa72c7e00856114ebe0dde7991b00e11bc8000b9
Oliver Klee <typo3-coding@oliverklee.de>
2019-11-19 23:41:49 +0100

    [CLEANUP] Streamline ext_localconf.php and ext_tables.php
    - reduce code duplication
    - stop polluting the global namespace

--- ext_tables.php
+++ ext_tables.php
@@ -1,24 +1,29 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_seminars_seminars',
-    'EXT:seminars/Resources/Private/Language/locallang_csh_seminars.xlf'
-);
+$boot = static function () {
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_seminars_seminars',
+        'EXT:seminars/Resources/Private/Language/locallang_csh_seminars.xlf'
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'fe_groups',
-    'EXT:seminars/Resources/Private/Language/locallang_csh_fe_groups.xlf'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'fe_groups',
+        'EXT:seminars/Resources/Private/Language/locallang_csh_fe_groups.xlf'
+    );
 
-$moduleConfiguration = [
-    'routeTarget' => \OliverKlee\Seminars\BackEnd\Controller::class . '::mainAction',
-    'access' => 'user,group',
-    'name' => 'web_seminars',
-    'labels' => [
-        'll_ref' => 'LLL:EXT:seminars/Resources/Private/Language/BackEnd/locallang_mod.xlf',
-    ],
-    'icon' => 'EXT:seminars/Resources/Public/Icons/BackEndModule.gif',
-];
+    $moduleConfiguration = [
+        'routeTarget' => \OliverKlee\Seminars\BackEnd\Controller::class . '::mainAction',
+        'access' => 'user,group',
+        'name' => 'web_seminars',
+        'labels' => [
+            'll_ref' => 'LLL:EXT:seminars/Resources/Private/Language/BackEnd/locallang_mod.xlf',
+        ],
+        'icon' => 'EXT:seminars/Resources/Public/Icons/BackEndModule.gif',
+    ];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('web', 'seminars', '', '', $moduleConfiguration);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('web', 'seminars', '', '', $moduleConfiguration);
+};
+
+$boot();
+unset($boot);
