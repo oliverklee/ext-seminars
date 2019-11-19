c3e6783ba418d319a60a5f845e5f44b2d9e428d7
Oliver Klee <typo3-coding@oliverklee.de>
2019-11-20 00:05:19 +0100

    [CLEANUP] Streamline ext_localconf.php and ext_tables.php (#327)
    - reduce code duplication
    - stop polluting the global namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,145 +1,101 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_seminars=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_speakers=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_attendances=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_sites=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_organizers=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_payment_methods=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_event_types=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_checkboxes=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_lodgings=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_foods=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_target_groups=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_categories=1
-'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '
-    options.saveDocNew.tx_seminars_skills=1
-'
-);
+$boot = static function () {
+    $tables = [
+        'seminars',
+        'speakers',
+        'attendances',
+        'sites',
+        'organizers',
+        'payment_method',
+        'event_types',
+        'checkboxes',
+        'lodging',
+        'foods',
+        'target_groups',
+        'categories',
+        'skills',
+    ];
+    foreach ($tables as $table) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+            \sprintf("\noptions.saveDocNew.tx_seminars_%s=1\n", $table)
+        );
+    }
 
-// Adds our custom function to a hook in \TYPO3\CMS\Core\DataHandling\DataHandler
-// Used for post-validation of fields in back-end forms.
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['seminars']
-    = \OliverKlee\Seminars\Hooks\DataHandlerHook::class;
+    // Adds our custom function to a hook in \TYPO3\CMS\Core\DataHandling\DataHandler
+    // Used for post-validation of fields in back-end forms.
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['seminars']
+        = \OliverKlee\Seminars\Hooks\DataHandlerHook::class;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
-    'seminars',
-    'Classes/FrontEnd/DefaultController.php',
-    '_pi1'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'seminars',
-    'setup',
-    '
-    plugin.' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN('seminars') . '_pi1.userFunc = Tx_Seminars_FrontEnd_DefaultController->main
-',
-    43
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
+        'seminars',
+        'Classes/FrontEnd/DefaultController.php',
+        '_pi1'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'seminars',
+        'setup',
+        '
+        plugin.' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN('seminars')
+        . '_pi1.userFunc = ' . \Tx_Seminars_FrontEnd_DefaultController::class . '->main
+        ',
+        43
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'seminars',
-    'setup',
-    '
-    tt_content.shortcut.20.conf.tx_seminars_seminars = < plugin.' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN(
-        'seminars'
-    ) . '_pi1
-    tt_content.shortcut.20.conf.tx_seminars_seminars.CMD = singleView
-',
-    43
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'seminars',
+        'setup',
+        '
+        tt_content.shortcut.20.conf.tx_seminars_seminars = < plugin.'
+        . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN('seminars') . '_pi1
+        tt_content.shortcut.20.conf.tx_seminars_seminars.CMD = singleView
+    ',
+        43
+    );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\OliverKlee\Seminars\SchedulerTasks\MailNotifier::class] = [
-    'extension' => 'seminars',
-    'title' => 'LLL:EXT:seminars/Resources/Private/Language/locallang.xlf:schedulerTasks.mailNotifier.name',
-    'description' => 'LLL:EXT:seminars/Resources/Private/Language/locallang.xlf:schedulerTasks.mailNotifier.description',
-    'additionalFields' => \OliverKlee\Seminars\SchedulerTasks\MailNotifierConfiguration::class,
-];
+    $languagePrefix = 'LLL:EXT:seminars/Resources/Private/Language/locallang.xlf:schedulerTasks.';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']
+    [\OliverKlee\Seminars\SchedulerTasks\MailNotifier::class] = [
+        'extension' => 'seminars',
+        'title' => $languagePrefix . 'mailNotifier.name',
+        'description' => $languagePrefix . 'mailNotifier.description',
+        'additionalFields' => \OliverKlee\Seminars\SchedulerTasks\MailNotifierConfiguration::class,
+    ];
 
-// RealURL auto-configuration
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['seminars']
-    = \OliverKlee\Seminars\RealUrl\Configuration::class . '->addConfiguration';
+    // RealURL auto-configuration
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']
+    ['seminars'] = \OliverKlee\Seminars\RealUrl\Configuration::class . '->addConfiguration';
 
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$provider = \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class;
-$prefix = 'EXT:seminars/Resources/Public/Icons/';
-$iconRegistry->registerIcon('tx-seminars-canceled', $provider, ['source' => $prefix . 'Canceled.png']);
-$iconRegistry->registerIcon('tx-seminars-category', $provider, ['source' => $prefix . 'Category.gif']);
-$iconRegistry->registerIcon('tx-seminars-registration', $provider, ['source' => $prefix . 'Registration.gif']);
-$iconRegistry->registerIcon('tx-seminars-event-complete', $provider, ['source' => $prefix . 'EventComplete.gif']);
-$iconRegistry->registerIcon('tx-seminars-event-topic', $provider, ['source' => $prefix . 'EventTopic.gif']);
-$iconRegistry->registerIcon('tx-seminars-event-date', $provider, ['source' => $prefix . 'EventDate.gif']);
-$iconRegistry->registerIcon(
-    'tcarecords-tx_seminars_speakers-default',
-    $provider,
-    ['source' => $prefix . 'Speaker.gif']
-);
-$iconRegistry->registerIcon(
-    'tcarecords-tx_seminars_organizers-default',
-    $provider,
-    ['source' => $prefix . 'Organizer.gif']
-);
-$iconRegistry->registerIcon('tcarecords-tx_seminars_registrations-default', $provider,
-    ['source' => $prefix . 'Registration.gif']
-);
-$iconRegistry->registerIcon(
-    'ext-seminars-wizard-icon',
-    $provider,
-    ['source' => 'EXT:seminars/Resources/Public/Icons/Extension.svg']
-);
+    $icons = [
+        'tx-seminars-canceled' => 'Canceled.png',
+        'tx-seminars-category' => 'Category.gif',
+        'tx-seminars-registration' => 'Registration.gif',
+        'tx-seminars-event-complete' => 'EventComplete.gif',
+        'tx-seminars-event-topic' => 'EventTopic.gif',
+        'tx-seminars-event-date' => 'EventDate.gif',
+        'tcarecords-tx_seminars_speakers-default' => 'Speaker.gif',
+        'tcarecords-tx_seminars_organizers-default' => 'Organizer.gif',
+        'tcarecords-tx_seminars_registrations-default' => 'Registration.gif',
+        'ext-seminars-wizard-icon' => 'Extension.svg',
+    ];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '
+    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    foreach ($icons as $key => $fileName) {
+        $iconRegistry->registerIcon(
+            $key,
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            ['source' => 'EXT:seminars/Resources/Public/Icons/' . $fileName]
+        );
+    }
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '
     <INCLUDE_TYPOSCRIPT: source="FILE:EXT:seminars/Configuration/TSconfig/ContentElementWizard.txt">
 '
-);
+    );
+};
+
+$boot();
+unset($boot);
