b373e63f6dc599970de18a11e291803e02213bbc
Michael Kramer <m.kramer@mxp.de>
2019-11-20 16:43:35 +0100

    Fix commenting grammar

--- Classes/Hooks/HookProvider.php
+++ Classes/Hooks/HookProvider.php
@@ -7,7 +7,7 @@ use OliverKlee\Seminars\Hooks\Interfaces\Hook;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * This class provides functions for unified hooks.
+ * Provides functions for unified hooks
  *
  * A hook allows adding functionality at certain points of the program path. These points are
  * grouped using an interface, declaring the methods to implement to hook in.
@@ -79,7 +79,7 @@ class HookProvider
     }
 
     /**
-     * Execute the hooked-in methods
+     * Executes the hooked-in methods
      *
      * @param string $method the method to execute
      * @param mixed $params parameters to $method()
@@ -107,7 +107,7 @@ class HookProvider
     }
 
     /**
-     * Gets the hook objects for the interface.
+     * Gets the hook objects for the interface
      *
      * @return Hook[]
      */
@@ -119,7 +119,7 @@ class HookProvider
     }
 
     /**
-     * Retrieves the hook objects for the interface.
+     * Retrieves the hook objects for the interface
      *
      * @return void
      *
