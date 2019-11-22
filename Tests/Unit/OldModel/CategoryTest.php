940025954055f43435e1e2da10c7a2e15549ea27
Oliver Klee <typo3-coding@oliverklee.de>
2019-11-22 20:37:09 +0100

    [TASK] Convert some functional old model tests to unit tests

---
+++ Tests/Unit/OldModel/CategoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\OldModel;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Seminars\OldModel\AbstractModel;
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+final class CategoryTest extends UnitTestCase
+{
+    /**
+     * @var \Tx_Seminars_OldModel_Category
+     */
+    private $subject = null;
+
+    protected function setUp()
+    {
+        $this->subject = new \Tx_Seminars_OldModel_Category();
+    }
+
+    /**
+     * @test
+     */
+    public function isAbstractModel()
+    {
+        self::assertInstanceOf(AbstractModel::class, $this->subject);
+    }
+}
---
+++ Tests/Unit/OldModel/OrganizerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\OldModel;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Seminars\OldModel\AbstractModel;
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+final class OrganizerTest extends UnitTestCase
+{
+    /**
+     * @var \Tx_Seminars_OldModel_Organizer
+     */
+    private $subject = null;
+
+    protected function setUp()
+    {
+        $this->subject = new \Tx_Seminars_OldModel_Organizer();
+    }
+
+    /**
+     * @test
+     */
+    public function isAbstractModel()
+    {
+        self::assertInstanceOf(AbstractModel::class, $this->subject);
+    }
+}
