0bb49c241318309de08b35db02976571a9cb928b
Oliver Klee <typo3-coding@oliverklee.de>
2019-11-28 10:45:20 +0100

    [TASK] Convert some fragile bag builder tests to the new TF
    Part of #384.

---
+++ Tests/Functional/BagBuilder/EventBagBuilderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Functional\BagBuilder;
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use OliverKlee\Seminars\Bag\AbstractBag;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Test case.
+ *
+ * @author Bernd Schönbach <bernd@oliverklee.de>
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+final class EventBagBuilderTest extends FunctionalTestCase
+{
+    /**
+     * @var string[]
+     */
+    protected $testExtensionsToLoad = ['typo3conf/ext/oelib', 'typo3conf/ext/seminars'];
+
+    /**
+     * @var \Tx_Seminars_BagBuilder_Event
+     */
+    private $subject = null;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->subject = new \Tx_Seminars_BagBuilder_Event();
+    }
+
+    /**
+     * @param int $uid
+     * @param AbstractBag $bag
+     *
+     * @return void
+     */
+    private static function assertBagContainsUid(int $uid, AbstractBag $bag)
+    {
+        $uids = GeneralUtility::intExplode(',', $bag->getUids(), true);
+        self::assertContains($uid, $uids);
+    }
+
+    /**
+     * @test
+     */
+    public function limitToEventsWithVacanciesForEventWithVacanciesAndOnlyOfflineAttendeesFindsThisEvent()
+    {
+        $this->importDataSet(__DIR__ . '/Fixtures/Records.xml');
+
+        $this->subject->limitToEventsWithVacancies();
+        $bag = $this->subject->build();
+
+        self::assertBagContainsUid(1, $bag);
+    }
+
+    /**
+     * @test
+     */
+    public function limitToEventsWithVacanciesForEventWithOneVacancyFindsThisEvent()
+    {
+        $this->importDataSet(__DIR__ . '/Fixtures/Records.xml');
+
+        $this->subject->limitToEventsWithVacancies();
+        $bag = $this->subject->build();
+
+        self::assertBagContainsUid(2, $bag);
+    }
+}
