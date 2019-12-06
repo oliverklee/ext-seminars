873794709f42edc643f84ea7377007e902a2eb1a
Oliver Klee <typo3-coding@oliverklee.de>
2019-12-06 22:19:44 +0100

    [BUGFIX] Use propert routes in the BE tab menu (#424)

--- Classes/BackEnd/AbstractList.php
+++ Classes/BackEnd/AbstractList.php
@@ -271,7 +271,7 @@ abstract class AbstractList
         $pageData = $this->page->getPageData();
         $csvLabel = $this->getLanguageService()->getLL('csvExport');
         $urlParameters = ['id' => (int)$pageData['uid'], 'csv' => '1', 'tx_seminars_pi2[table]' => $this->tableName];
-        $csvUrl = BackendUtility::getModuleUrl(self::MODULE_NAME, $urlParameters);
+        $csvUrl = $this->getRouteUrl(self::MODULE_NAME, $urlParameters);
 
         return '<div id="typo3-csvLink">' .
             '<a class="btn btn-default" href="' . \htmlspecialchars($csvUrl, ENT_QUOTES | ENT_HTML5) .
