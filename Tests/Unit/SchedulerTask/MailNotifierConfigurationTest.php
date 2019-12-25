ebc18393b1951b4e214a135063c8e38d6e285ce3
Oliver Klee <typo3-coding@oliverklee.de>
2019-12-25 17:59:28 +0100

    [TASK] Convert MailNotifierConfigurationTest to the new TF

---
+++ Tests/Unit/SchedulerTask/MailNotifierConfigurationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\SchedulerTasks;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Seminars\SchedulerTasks\MailNotifierConfiguration;
+use TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface;
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class MailNotifierConfigurationTest extends UnitTestCase
+{
+    /**
+     * @var MailNotifierConfiguration
+     */
+    private $subject = null;
+
+    protected function setUp()
+    {
+        $this->subject = new MailNotifierConfiguration();
+    }
+
+    /**
+     * @test
+     */
+    public function classImplementsAdditionalFieldProvider()
+    {
+        self::assertInstanceOf(AdditionalFieldProviderInterface::class, $this->subject);
+    }
+}
