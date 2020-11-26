0a49df24a408b3c5d940822a70d6a7ff5a9d2e4b
Oliver Klee <typo3-coding@oliverklee.de>
2020-11-26 19:30:05 +0100

    [TASK] Update the dependencies
    Fixes #558

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,9 +9,9 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '7.0.0-7.2.99',
             'typo3' => '8.7.0-9.5.99',
-            'static_info_tables' => '6.7.5-',
-            'oelib' => '3.0.3-3.99.99',
-            'mkforms' => '9.5.2-9.5.99',
+            'oelib' => '3.1.0-3.99.99',
+            'mkforms' => '9.5.4-9.5.99',
+            'static_info_tables' => '6.8.0-6.99.99',
         ],
         'conflicts' => [
             'sourceopt' => '',
