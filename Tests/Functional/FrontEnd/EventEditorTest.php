e4153f22fbe1b956507f9fafb9143cb2e1339847
Stefano Kowalke <Konafets@users.noreply.github.com>
2021-01-26 19:55:01 +0100

    [CLEANUP] Stop calling ->initTemplate in TYPO3 >= 9 (#716)
    Closes #700

--- Tests/Functional/FrontEnd/EventEditorTest.php
+++ Tests/Functional/FrontEnd/EventEditorTest.php
@@ -7,6 +7,7 @@ namespace OliverKlee\Seminars\Tests\Functional\FrontEnd;
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use OliverKlee\Oelib\Authentication\FrontEndLoginManager;
 use OliverKlee\Oelib\Mapper\MapperRegistry;
+use OliverKlee\Oelib\System\Typo3Version;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
@@ -90,7 +91,9 @@ final class EventEditorTest extends FunctionalTestCase
         }
         $frontEndController->determineId();
         $frontEndController->cObj = $contentObject;
-        $frontEndController->initTemplate();
+        if (Typo3Version::isNotHigherThan(8)) {
+            $frontEndController->initTemplate();
+        }
 
         $this->frontEndController = $frontEndController;
         $GLOBALS['TSFE'] = $frontEndController;
