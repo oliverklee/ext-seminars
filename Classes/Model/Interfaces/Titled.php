77b722bddbcb13e5f0aa28f13ebf9eab56adf161
Oliver Klee <github@oliverklee.de>
2021-09-16 00:50:57 +0200

    [BUGFIX] Remove duplicate classes (#852)
    Those were introducted by accident by #623.
    
    Fixes #851

--- Classes/Model/Interfaces/Titled.php
+++ Classes/Model/Interfaces/Titled.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace OliverKlee\Seminars\Model\Interfaces;
 
 /**
- * This interface is used for models having a title and is needed for the CommaSeparatedTitles view helper.
+ * This interface is used for models having a title and is needed for the `CommaSeparatedTitles` view helper.
  *
  * @author Niels Pardon <mail@niels-pardon.de>
  */
