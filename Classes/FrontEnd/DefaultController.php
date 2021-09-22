7d69d18d8f5f875d002e08e6dd0a1e76c77e0eec
Oliver Klee <github@oliverklee.de>
2021-09-22 19:39:56 +0200

    [CLEANUP] Switch `allowRegistrationForEventsWithoutDate` to the new config (#917)

--- Classes/FrontEnd/DefaultController.php
+++ Classes/FrontEnd/DefaultController.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use OliverKlee\Oelib\Authentication\FrontEndLoginManager;
+use OliverKlee\Oelib\Configuration\ConfigurationRegistry;
 use OliverKlee\Oelib\DataStructures\Collection;
 use OliverKlee\Oelib\Http\HeaderProxyFactory;
+use OliverKlee\Oelib\Interfaces\Configuration;
 use OliverKlee\Oelib\Interfaces\ConfigurationCheckable;
 use OliverKlee\Oelib\Mapper\MapperRegistry;
 use OliverKlee\Oelib\Templating\TemplateHelper;
@@ -56,6 +58,11 @@ class Tx_Seminars_FrontEnd_DefaultController extends TemplateHelper implements C
      * @var \Tx_Seminars_Service_ConfigurationService
      */
     private $configurationService = null;
+
+    /**
+     * @var Configuration|null
+     */
+    private $configuration = null;
 
     /**
      * @var \Tx_Seminars_OldModel_Event|null the seminar which we want to list/show or
@@ -2333,7 +2340,7 @@ class Tx_Seminars_FrontEnd_DefaultController extends TemplateHelper implements C
         if (
             !$event->needsRegistration()
             || (!$event->hasDate()
-                && !$this->configurationService->getConfValueBoolean('allowRegistrationForEventsWithoutDate'))
+                && !$this->getSharedConfiguration()->getAsBoolean('allowRegistrationForEventsWithoutDate'))
         ) {
             return '';
         }
@@ -3522,5 +3529,14 @@ class Tx_Seminars_FrontEnd_DefaultController extends TemplateHelper implements C
     public function getTypoScriptNamespace(): string
     {
         return 'plugin.tx_seminars_pi1.';
+    }
+
+    private function getSharedConfiguration(): Configuration
+    {
+        if (!$this->configuration instanceof Configuration) {
+            $this->configuration = ConfigurationRegistry::get('plugin.tx_seminars');
+        }
+
+        return $this->configuration;
     }
 }
