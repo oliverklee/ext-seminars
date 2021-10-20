97389a919792d3e8c9e069bf8ff1e59b68eb4465
Oliver Klee <typo3-coding@oliverklee.de>
2021-10-20 17:51:50 +0200

    [BUGFIX] Make the testing email trait V10-compatible
    Part of #1109

--- Tests/Unit/Traits/EmailTrait.php
+++ Tests/Unit/Traits/EmailTrait.php
@@ -6,10 +6,11 @@ namespace OliverKlee\Seminars\Tests\Unit\Traits;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Mime\Address;
 use TYPO3\CMS\Core\Mail\MailMessage;
 
 /**
- * Helper for creating email mocks.
+ * Helper for writing tests for sending emails.
  *
  * @mixin TestCase
  */
@@ -34,5 +35,90 @@ trait EmailTrait
             ->getMock();
 
         return $message;
+    }
+
+    /**
+     * Returns the recipients of the given email message in an array using the email addresses as keys and names
+     * as values.
+     *
+     * @return array<string, string>
+     */
+    private function getToOfEmail(MailMessage $message): array
+    {
+        // @phpstan-ignore-next-line This line is V9-specific, and we are running PHPStan with V10.
+        if ($message instanceof \Swift_Message) {
+            $addresses = $message->getTo();
+        } else {
+            $addresses = $this->addressesToArray($message->getTo());
+        }
+
+        return $addresses;
+    }
+
+    /**
+     * Returns the sender(s) of the given email message in an array using the email addresses as keys and names
+     * as values.
+     *
+     * @return array<string, string>
+     */
+    private function getFromOfEmail(MailMessage $message): array
+    {
+        // @phpstan-ignore-next-line This line is V9-specific, and we are running PHPStan with V10.
+        if ($message instanceof \Swift_Message) {
+            $addresses = $message->getFrom();
+        } else {
+            $addresses = $this->addressesToArray($message->getFrom());
+        }
+
+        return $addresses;
+    }
+
+    /**
+     * Returns the reply-tos of the given email message in an array using the email addresses as keys and names
+     * as values.
+     *
+     * @return array<string, string>
+     */
+    private function getReplyToOfEmail(MailMessage $message): array
+    {
+        // @phpstan-ignore-next-line This line is V9-specific, and we are running PHPStan with V10.
+        if ($message instanceof \Swift_Message) {
+            /** @var array<string, string> $addresses */
+            $addresses = $message->getReplyTo();
+        } else {
+            $addresses = $this->addressesToArray($message->getReplyTo());
+        }
+
+        return $addresses;
+    }
+
+    /**
+     * @param array<array-key, Address> $addresses
+     *
+     * @return array<string, string> keys: email addresses, values: names
+     */
+    private function addressesToArray(array $addresses): array
+    {
+        $plainAddresses = [];
+        foreach ($addresses as $address) {
+            $plainAddresses[$address->getAddress()] = $address->getName();
+        }
+        return $plainAddresses;
+    }
+
+    /**
+     * Returns plain-text content of an email.
+     */
+    private function getTextBodyOfEmail(MailMessage $message): string
+    {
+        // @phpstan-ignore-next-line This line is V9-specific, and we are running PHPStan with V10.
+        if ($message instanceof \Swift_Message) {
+            /** @var array<string, string> $text */
+            $text = $message->getBody();
+        } else {
+            $text = $message->getTextBody();
+        }
+
+        return $text;
     }
 }
