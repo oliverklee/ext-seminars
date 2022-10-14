deb98769deedc9b5ac348dbf75b0742b9f3b628a
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-14 18:39:49 +0200

    [FEATURE] Add a Speaker model and repository

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    OliverKlee\Seminars\Domain\Model\Speaker::class => [
+        'tableName' => 'tx_seminars_speakers',
+        'properties' => [
+            'name' => ['fieldName' => 'title'],
+            'emailAddress' => ['fieldName' => 'email'],
+        ],
+    ],
+];
