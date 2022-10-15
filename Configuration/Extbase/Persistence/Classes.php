f7c9895b88fbfb504eea70a9b0499562ff39bf7d
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-15 18:13:28 +0200

    [FEATURE] Add an EventType model and repository (#1575)

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 return [
+    OliverKlee\Seminars\Domain\Model\EventType::class => [
+        'tableName' => 'tx_seminars_event_types',
+    ],
     OliverKlee\Seminars\Domain\Model\Speaker::class => [
         'tableName' => 'tx_seminars_speakers',
         'properties' => [
---
+++ Tests/Unit/Domain/Model/EventTypeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\Domain\Model;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Seminars\Domain\Model\EventType;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * @covers \OliverKlee\Seminars\Domain\Model\EventType
+ */
+final class EventTypeTest extends UnitTestCase
+{
+    /**
+     * @var EventType
+     */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new EventType();
+    }
+
+    /**
+     * @test
+     */
+    public function isAbstractEntity(): void
+    {
+        self::assertInstanceOf(AbstractEntity::class, $this->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function getTitleInitiallyReturnsEmptyString(): void
+    {
+        self::assertSame('', $this->subject->getTitle());
+    }
+
+    /**
+     * @test
+     */
+    public function setTitleSetsTitle(): void
+    {
+        $value = 'workshop';
+        $this->subject->setTitle($value);
+
+        self::assertSame($value, $this->subject->getTitle());
+    }
+}
