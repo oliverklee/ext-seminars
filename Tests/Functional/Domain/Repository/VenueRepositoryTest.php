0ec1f0ab652d48ba2664e62d972c76cf7f1bb030
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-17 14:15:12 +0200

    [FEATURE] Add a `Venue` model and repository (#1586)

---
+++ Tests/Functional/Domain/Repository/VenueRepositoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Functional\Domain\Repository;
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use OliverKlee\Seminars\Domain\Model\Venue;
+use OliverKlee\Seminars\Domain\Repository\VenueRepository;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+
+/**
+ * @covers \OliverKlee\Seminars\Domain\Model\Venue
+ * @covers \OliverKlee\Seminars\Domain\Repository\VenueRepository
+ */
+final class VenueRepositoryTest extends FunctionalTestCase
+{
+    protected $testExtensionsToLoad = ['typo3conf/ext/oelib', 'typo3conf/ext/seminars'];
+
+    /**
+     * @var VenueRepository
+     */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ((new Typo3Version())->getMajorVersion() >= 11) {
+            $this->subject = GeneralUtility::makeInstance(VenueRepository::class);
+        } else {
+            $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+            $this->subject = $objectManager->get(VenueRepository::class);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function mapsAllModelFields(): void
+    {
+        $this->importDataSet(__DIR__ . '/Fixtures/VenueRepository/VenueWithAllFields.xml');
+
+        $result = $this->subject->findByUid(1);
+
+        self::assertInstanceOf(Venue::class, $result);
+        self::assertSame('JH Köln-Deutz', $result->getTitle());
+    }
+}
---
+++ Tests/Unit/Domain/Repository/VenueRepositoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\Domain\Repository;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Seminars\Domain\Repository\VenueRepository;
+use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * @covers \OliverKlee\Seminars\Domain\Repository\VenueRepository
+ */
+final class VenueRepositoryTest extends UnitTestCase
+{
+    /**
+     * @var VenueRepository
+     */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $objectManagerStub = $this->prophesize(ObjectManagerInterface::class)->reveal();
+        $this->subject = new VenueRepository($objectManagerStub);
+    }
+
+    /**
+     * @test
+     */
+    public function isRepository(): void
+    {
+        self::assertInstanceOf(Repository::class, $this->subject);
+    }
+}
