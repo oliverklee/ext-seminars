d2b99197d8cddabd8fae8a0d6201e195fc7c3453
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-17 17:16:37 +0200

    [FEATURE] Add an `EventInterface`

---
+++ Classes/Domain/Model/EventTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Domain\Model;
+
+use TYPO3\CMS\Extbase\Annotation as Extbase;
+
+/**
+ * Trait to avoid code duplication for some methods of `Event` classes.
+ *
+ * @mixin EventInterface
+ */
+trait EventTrait
+{
+    /**
+     * The title of this event as visible in the backend.
+     * In the frontend, the title might be different, e.g., event dates will use the title of their
+     * corresponding topic.
+     *
+     * @var string
+     * @Extbase\Validate("StringLength", options={"maximum": 255})
+     */
+    protected $internalTitle = '';
+
+    public function getInternalTitle(): string
+    {
+        return $this->internalTitle;
+    }
+
+    public function setInternalTitle(string $name): void
+    {
+        $this->internalTitle = $name;
+    }
+}
