da8fcc78971887540d9e38ecca5e9a801221e10e
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-17 15:09:22 +0200

    [FEATURE] Add an `Organizer` model and repository (#1590)

---
+++ Classes/Domain/Repository/OrganizerRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Domain\Repository;
+
+use OliverKlee\Seminars\Domain\Model\Organizer;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * @extends Repository<Organizer>
+ */
+class OrganizerRepository extends Repository
+{
+}
