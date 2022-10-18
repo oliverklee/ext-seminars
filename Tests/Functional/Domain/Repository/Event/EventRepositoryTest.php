1b9886f69af609ad367fe28b6d0e0d6f3f024fa2
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-18 12:47:51 +0200

    [FEATURE] Add tests for persisting STI event records
    Fixes #1606

--- Tests/Functional/Domain/Repository/Event/EventRepositoryTest.php
+++ Tests/Functional/Domain/Repository/Event/EventRepositoryTest.php
@@ -6,12 +6,14 @@ namespace OliverKlee\Seminars\Tests\Functional\Domain\Repository\Event;
 
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use OliverKlee\Seminars\Domain\Model\Event\EventDate;
+use OliverKlee\Seminars\Domain\Model\Event\EventInterface;
 use OliverKlee\Seminars\Domain\Model\Event\EventTopic;
 use OliverKlee\Seminars\Domain\Model\Event\SingleEvent;
 use OliverKlee\Seminars\Domain\Repository\Event\EventRepository;
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 
 /**
  * @covers \OliverKlee\Seminars\Domain\Model\Event\Event
@@ -29,6 +31,11 @@ final class EventRepositoryTest extends FunctionalTestCase
      */
     private $subject;
 
+    /**
+     * @var PersistenceManager
+     */
+    private $persistenceManager;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -39,6 +46,8 @@ final class EventRepositoryTest extends FunctionalTestCase
             $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
             $this->subject = $objectManager->get(EventRepository::class);
         }
+
+        $this->persistenceManager = GeneralUtility::makeInstance(PersistenceManager::class);
     }
 
     /**
@@ -78,5 +87,71 @@ final class EventRepositoryTest extends FunctionalTestCase
 
         self::assertInstanceOf(EventDate::class, $result);
         self::assertSame('Jousting date', $result->getInternalTitle());
+    }
+
+    /**
+     * @test
+     */
+    public function persistsSingleEventWithSingleEventRecordType(): void
+    {
+        $event = new SingleEvent();
+        $this->subject->add($event);
+        $this->persistenceManager->persistAll();
+
+        $connection = $this->getConnectionPool()->getConnectionForTable('tx_seminars_seminars');
+        $result = $connection
+            ->executeQuery('SELECT * FROM tx_seminars_seminars WHERE uid = :uid', ['uid' => $event->getUid()]);
+        if (\method_exists($result, 'fetchAssociative')) {
+            $databaseRow = $result->fetchAssociative();
+        } else {
+            $databaseRow = $result->fetch();
+        }
+
+        self::assertIsArray($databaseRow);
+        self::assertSame(EventInterface::TYPE_SINGLE_EVENT, $databaseRow['object_type']);
+    }
+
+    /**
+     * @test
+     */
+    public function persistsEventTopicWithEventTopicType(): void
+    {
+        $event = new EventTopic();
+        $this->subject->add($event);
+        $this->persistenceManager->persistAll();
+
+        $connection = $this->getConnectionPool()->getConnectionForTable('tx_seminars_seminars');
+        $result = $connection
+            ->executeQuery('SELECT * FROM tx_seminars_seminars WHERE uid = :uid', ['uid' => $event->getUid()]);
+        if (\method_exists($result, 'fetchAssociative')) {
+            $databaseRow = $result->fetchAssociative();
+        } else {
+            $databaseRow = $result->fetch();
+        }
+
+        self::assertIsArray($databaseRow);
+        self::assertSame(EventInterface::TYPE_EVENT_TOPIC, $databaseRow['object_type']);
+    }
+
+    /**
+     * @test
+     */
+    public function persistsEventDateWithEventDateType(): void
+    {
+        $event = new EventDate();
+        $this->subject->add($event);
+        $this->persistenceManager->persistAll();
+
+        $connection = $this->getConnectionPool()->getConnectionForTable('tx_seminars_seminars');
+        $result = $connection
+            ->executeQuery('SELECT * FROM tx_seminars_seminars WHERE uid = :uid', ['uid' => $event->getUid()]);
+        if (\method_exists($result, 'fetchAssociative')) {
+            $databaseRow = $result->fetchAssociative();
+        } else {
+            $databaseRow = $result->fetch();
+        }
+
+        self::assertIsArray($databaseRow);
+        self::assertSame(EventInterface::TYPE_EVENT_DATE, $databaseRow['object_type']);
     }
 }
