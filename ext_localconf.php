a2b2e0dd5fd8f7736827bed7aa7c58e351a0ebd0
Oliver Klee <typo3-coding@oliverklee.de>
2022-10-27 13:45:34 +0200

    [FEATURE] Add an FE edit form with text fields
    Part of #1513

--- ext_localconf.php
+++ ext_localconf.php
@@ -110,11 +110,11 @@ defined('TYPO3_MODE') or die('Access denied.');
             'FrontEndEditor',
             // all actions
             [
-                \OliverKlee\Seminars\Controller\FrontEndEditorController::class => 'index',
+                \OliverKlee\Seminars\Controller\FrontEndEditorController::class => 'index, edit, update',
             ],
             // non-cacheable actions
             [
-                \OliverKlee\Seminars\Controller\FrontEndEditorController::class => 'index',
+                \OliverKlee\Seminars\Controller\FrontEndEditorController::class => 'index, edit, update',
             ]
         );
     }
