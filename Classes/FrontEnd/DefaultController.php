9a19bfe9c5350b8b552647058b30ba9ca5cd10b6
Oliver Klee <typo3-coding@oliverklee.de>
2022-11-15 18:18:34 +0100

    [FEATURE] Adapt the registration links to the new registration form

--- Classes/FrontEnd/DefaultController.php
+++ Classes/FrontEnd/DefaultController.php
@@ -562,14 +562,24 @@ class DefaultController extends TemplateHelper
     {
         $linkConfiguration = ['parameter' => $pageId];
 
-        if ($eventId) {
-            $linkConfiguration['additionalParams'] = GeneralUtility::implodeArrayForUrl(
-                'tx_seminars_pi1',
-                ['seminar' => $eventId, 'action' => 'register'],
-                '',
-                false,
-                true
-            );
+        if ($eventId > 0) {
+            $linkConfiguration['additionalParams']
+                // @deprecated These parameters can be removed in seminars 5.0
+                // when the legacy registration form is removed.
+                = GeneralUtility::implodeArrayForUrl(
+                    'tx_seminars_pi1',
+                    ['seminar' => $eventId, 'action' => 'register'],
+                    '',
+                    false,
+                    true
+                ) . '&' .
+                GeneralUtility::implodeArrayForUrl(
+                    'tx_seminars_eventregistration',
+                    ['event' => $eventId],
+                    '',
+                    false,
+                    true
+                );
         }
 
         $redirectUrl = GeneralUtility::locationHeaderUrl(
