30addcd9be24ba69894845cf573b732bfdcceadb
Oliver Klee <typo3-coding@oliverklee.de>
2022-11-19 11:22:48 +0100

    [FEATURE] Pass a `Registration` around in the registration form (#1892)

--- Tests/Unit/Controller/EventRegistrationControllerTest.php
+++ Tests/Unit/Controller/EventRegistrationControllerTest.php
@@ -8,8 +8,10 @@ use Nimut\TestingFramework\MockObject\AccessibleMockObjectInterface;
 use Nimut\TestingFramework\TestCase\UnitTestCase;
 use OliverKlee\Seminars\Controller\EventRegistrationController;
 use OliverKlee\Seminars\Domain\Model\Event\SingleEvent;
+use OliverKlee\Seminars\Domain\Model\Registration\Registration;
 use OliverKlee\Seminars\Service\RegistrationGuard;
 use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Mvc\Exception\StopActionException;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
@@ -59,6 +61,12 @@ final class EventRegistrationControllerTest extends UnitTestCase
 
         $this->uriBuilderMock = $this->createMock(UriBuilder::class);
         $this->subject->_set('uriBuilder', $this->uriBuilderMock);
+    }
+
+    protected function tearDown(): void
+    {
+        GeneralUtility::purgeInstances();
+        parent::tearDown();
     }
 
     /**
@@ -170,7 +178,8 @@ final class EventRegistrationControllerTest extends UnitTestCase
         $this->registrationGuardMock->method('isRegistrationPossibleAtAnyTimeAtAll')->with($event)->willReturn(true);
         $this->registrationGuardMock->method('isRegistrationPossibleByDate')->with($event)->willReturn(true);
         $this->registrationGuardMock->method('existsFrontEndUserUidInSession')->willReturn(true);
-        $this->registrationGuardMock->method('isFreeFromRegistrationConflicts')->with($event, $userUid)->willReturn(true);
+        $this->registrationGuardMock->method('isFreeFromRegistrationConflicts')
+            ->with($event, $userUid)->willReturn(true);
 
         $this->subject->expects(self::once())->method('redirect')
             ->with('new', null, null, ['event' => $event])
@@ -233,9 +242,59 @@ final class EventRegistrationControllerTest extends UnitTestCase
     {
         $event = new SingleEvent();
 
-        $this->viewMock->expects(self::once())->method('assign')->with('event', $event);
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', $event],
+            ['registration', self::anything()]
+        );
 
-        $this->subject->newAction($event);
+        $this->subject->newAction($event, new Registration());
+    }
+
+    /**
+     * @test
+     */
+    public function newActionPassesProvidedRegistrationToView(): void
+    {
+        $registration = new Registration();
+
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', self::anything()],
+            ['registration', $registration]
+        );
+
+        $this->subject->newAction(new SingleEvent(), $registration);
+    }
+
+    /**
+     * @test
+     */
+    public function newActionWithNullRegistrationPassesNewRegistrationToView(): void
+    {
+        $registration = new Registration();
+        GeneralUtility::addInstance(Registration::class, $registration);
+
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', self::anything()],
+            ['registration', $registration]
+        );
+
+        $this->subject->newAction(new SingleEvent(), null);
+    }
+
+    /**
+     * @test
+     */
+    public function newActionWithoutRegistrationPassesNewRegistrationToView(): void
+    {
+        $registration = new Registration();
+        GeneralUtility::addInstance(Registration::class, $registration);
+
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', self::anything()],
+            ['registration', $registration]
+        );
+
+        $this->subject->newAction(new SingleEvent(), null);
     }
 
     /**
@@ -245,9 +304,27 @@ final class EventRegistrationControllerTest extends UnitTestCase
     {
         $event = new SingleEvent();
 
-        $this->viewMock->expects(self::once())->method('assign')->with('event', $event);
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', $event],
+            ['registration', self::anything()]
+        );
 
-        $this->subject->confirmAction($event);
+        $this->subject->confirmAction($event, new Registration());
+    }
+
+    /**
+     * @test
+     */
+    public function confirmActionPassesProvidedRegistrationToView(): void
+    {
+        $registration = new Registration();
+
+        $this->viewMock->expects(self::exactly(2))->method('assign')->withConsecutive(
+            ['event', self::anything()],
+            ['registration', $registration]
+        );
+
+        $this->subject->confirmAction(new SingleEvent(), $registration);
     }
 
     /**
@@ -262,7 +339,7 @@ final class EventRegistrationControllerTest extends UnitTestCase
             ->willThrowException(new StopActionException('redirectToUri', 1476045828));
         $this->expectException(StopActionException::class);
 
-        $this->subject->createAction($event);
+        $this->subject->createAction($event, new Registration());
     }
 
     /**
