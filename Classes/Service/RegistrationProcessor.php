98de113beaf681912f37b89ef680f771f3250ba9
Oliver Klee <typo3-coding@oliverklee.de>
2022-11-19 20:18:51 +0100

    [FEATURE] Add a `RegistrationProcessor` (#1902)

---
+++ Classes/Service/RegistrationProcessor.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Service;
+
+use OliverKlee\FeUserExtraFields\Domain\Model\FrontendUser;
+use OliverKlee\FeUserExtraFields\Domain\Repository\FrontendUserRepository;
+use OliverKlee\Seminars\Domain\Model\Event\Event;
+use OliverKlee\Seminars\Domain\Model\Registration\Registration;
+use OliverKlee\Seminars\Domain\Repository\Registration\RegistrationRepository;
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * Takes care of enriching and processing a registration after an attendee has registered for an event.
+ *
+ * This is the recommended way to process a registration:
+ * 1. `enrichWithMetadata`
+ * 2. `persist`
+ */
+class RegistrationProcessor implements SingletonInterface
+{
+    /**
+     * @var RegistrationRepository
+     */
+    private $registrationRepository;
+
+    /**
+     * @var FrontendUserRepository
+     */
+    private $frontendUserRepository;
+
+    /**
+     * @var RegistrationGuard
+     */
+    private $registrationGuard;
+
+    public function injectRegistrationRepository(RegistrationRepository $repository): void
+    {
+        $this->registrationRepository = $repository;
+    }
+
+    public function injectFrontendUserRepository(FrontendUserRepository $repository): void
+    {
+        $this->frontendUserRepository = $repository;
+    }
+
+    public function injectRegistrationGuard(RegistrationGuard $registrationGuard): void
+    {
+        $this->registrationGuard = $registrationGuard;
+    }
+
+    /**
+     * Enriches the provided registration with the event and the user associations and a PID.
+     *
+     * Call this method before persisting the registration.
+     *
+     * @param array{registrationRecordsStorageFolder?: numeric-string} $settings
+     */
+    public function enrichWithMetadata(Registration $registration, Event $event, array $settings): void
+    {
+        $registration->setEvent($event);
+
+        $userUid = $this->registrationGuard->getFrontEndUserUidFromSession();
+        if (!\is_int($userUid)) {
+            throw new \RuntimeException('No user UID found in the session.', 1668865776);
+        }
+        $user = $this->frontendUserRepository->findByUid($userUid);
+        if (!$user instanceof FrontendUser) {
+            throw new \RuntimeException('User with UID ' . $userUid . ' not found.', 1668865839);
+        }
+        $registration->setUser($user);
+
+        $folderUid = (int)($settings['registrationRecordsStorageFolder'] ?? 0);
+        $registration->setPid($folderUid);
+    }
+
+    /**
+     * Persists a registration. Call `enrichWithMetadata` first.
+     */
+    public function persist(Registration $registration): void
+    {
+        $this->registrationRepository->add($registration);
+        $this->registrationRepository->persistAll();
+    }
+}
---
+++ Tests/Unit/Service/RegistrationProcessorTest.php
@@ -0,0 +1,172 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\Unit\Service;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\FeUserExtraFields\Domain\Model\FrontendUser;
+use OliverKlee\FeUserExtraFields\Domain\Repository\FrontendUserRepository;
+use OliverKlee\Seminars\Domain\Model\Event\SingleEvent;
+use OliverKlee\Seminars\Domain\Model\Registration\Registration;
+use OliverKlee\Seminars\Domain\Repository\Registration\RegistrationRepository;
+use OliverKlee\Seminars\Service\RegistrationGuard;
+use OliverKlee\Seminars\Service\RegistrationProcessor;
+use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * @covers \OliverKlee\Seminars\Service\RegistrationProcessor
+ */
+final class RegistrationProcessorTest extends UnitTestCase
+{
+    /**
+     * @var RegistrationRepository&MockObject
+     */
+    private $registrationRepositoryMock;
+
+    /**
+     * @var FrontEndUserRepository&MockObject
+     */
+    private $frontendUserRepositoryMock;
+
+    /**
+     * @var RegistrationGuard&MockObject
+     */
+    private $registrationGuardMock;
+
+    /**
+     * @var RegistrationProcessor
+     */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new RegistrationProcessor();
+
+        $this->registrationRepositoryMock = $this->createMock(RegistrationRepository::class);
+        $this->subject->injectRegistrationRepository($this->registrationRepositoryMock);
+        $this->frontendUserRepositoryMock = $this->createMock(FrontendUserRepository::class);
+        $this->subject->injectFrontendUserRepository($this->frontendUserRepositoryMock);
+        $this->registrationGuardMock = $this->createMock(RegistrationGuard::class);
+        $this->subject->injectRegistrationGuard($this->registrationGuardMock);
+    }
+
+    protected function tearDown(): void
+    {
+        GeneralUtility::purgeInstances();
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function isSingleton(): void
+    {
+        self::assertInstanceOf(SingletonInterface::class, $this->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function enrichWithMetadataSetsEvent(): void
+    {
+        $event = new SingleEvent();
+        $registration = new Registration();
+        $this->registrationGuardMock->method('getFrontEndUserUidFromSession')->willReturn(15);
+        $this->frontendUserRepositoryMock->method('findByUid')->with(self::anything())->willReturn(new FrontendUser());
+
+        $this->subject->enrichWithMetadata($registration, $event, []);
+
+        self::assertSame($event, $registration->getEvent());
+    }
+
+    /**
+     * @test
+     */
+    public function enrichWithMetadataSetsUserFromSession(): void
+    {
+        $userUid = 99;
+        $registration = new Registration();
+        $user = new FrontendUser();
+        $this->registrationGuardMock->expects(self::once())->method('getFrontEndUserUidFromSession')
+            ->willReturn($userUid);
+        $this->frontendUserRepositoryMock->expects(self::once())->method('findByUid')
+            ->with($userUid)->willReturn($user);
+
+        $this->subject->enrichWithMetadata($registration, new SingleEvent(), []);
+
+        self::assertSame($user, $registration->getUser());
+    }
+
+    /**
+     * @test
+     */
+    public function enrichWithMetadataWithoutUserUidInSessionThrowsException(): void
+    {
+        $registration = new Registration();
+        $this->registrationGuardMock->expects(self::once())->method('getFrontEndUserUidFromSession')
+            ->willReturn(null);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(1668865776);
+        $this->expectExceptionMessage('No user UID found in the session.');
+
+        $this->subject->enrichWithMetadata($registration, new SingleEvent(), []);
+    }
+
+    /**
+     * @test
+     */
+    public function enrichWithMetadataWithUserUidInSessionForInexistentUserThrowsException(): void
+    {
+        $userUid = 99;
+        $registration = new Registration();
+        $this->registrationGuardMock->expects(self::once())->method('getFrontEndUserUidFromSession')
+            ->willReturn($userUid);
+        $this->frontendUserRepositoryMock->expects(self::once())->method('findByUid')
+            ->with($userUid)->willReturn(null);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(1668865839);
+        $this->expectExceptionMessage('User with UID ' . $userUid . ' not found.');
+
+        $this->subject->enrichWithMetadata($registration, new SingleEvent(), []);
+    }
+
+    /**
+     * @test
+     */
+    public function enrichWithMetadataSetsFolderFromConfiguration(): void
+    {
+        $folderUid = 21;
+        $registration = new Registration();
+        $this->registrationGuardMock->method('getFrontEndUserUidFromSession')->willReturn(15);
+        $this->frontendUserRepositoryMock->method('findByUid')->with(self::anything())->willReturn(new FrontendUser());
+
+        $this->subject->enrichWithMetadata(
+            $registration,
+            new SingleEvent(),
+            ['registrationRecordsStorageFolder' => (string)$folderUid]
+        );
+
+        self::assertSame($folderUid, $registration->getPid());
+    }
+
+    /**
+     * @test
+     */
+    public function persistPersistsRegistration(): void
+    {
+        $registration = new Registration();
+
+        $this->registrationRepositoryMock->expects(self::once())->method('add')->with($registration);
+        $this->registrationRepositoryMock->expects(self::once())->method('persistAll');
+
+        $this->subject->persist($registration);
+    }
+}
