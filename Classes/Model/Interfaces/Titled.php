cd9563312cfac572752b4f1060bc13a81b938945
Oliver Klee <typo3-coding@oliverklee.de>
2022-11-21 16:29:08 +0100

    [TASK] Deprecate the `Titled` interface
    Fixes #1909
    
    [ci skip]

--- Classes/Model/Interfaces/Titled.php
+++ Classes/Model/Interfaces/Titled.php
@@ -6,6 +6,8 @@ namespace OliverKlee\Seminars\Model\Interfaces;
 
 /**
  * This interface is used for models having a title and is needed for the `CommaSeparatedTitles` view helper.
+ *
+ * @deprecated #1910 will be removed in seminars 5.0
  */
 interface Titled
 {
