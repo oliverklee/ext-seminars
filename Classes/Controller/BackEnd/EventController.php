0b0b8b92396f7b540e994f5a5bbb26ada1804619
Oliver Klee <typo3-coding@oliverklee.de>
2022-12-03 18:53:06 +0100

    [FEATURE] Provide access to the page tree UID in the BE module (#2077)

--- Classes/Controller/BackEnd/EventController.php
+++ Classes/Controller/BackEnd/EventController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace OliverKlee\Seminars\Controller\BackEnd;
 
 use OliverKlee\Seminars\BackEnd\Permissions;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
@@ -22,8 +23,19 @@ class EventController extends ActionController
         $this->permissions = $permissions;
     }
 
+    /**
+     * This method is only public for unit testing.
+     *
+     * @return 0|positive-int
+     */
+    public function getPageUid(): int
+    {
+        return (int)(GeneralUtility::_GP('id') ?? 0);
+    }
+
     public function indexAction(): void
     {
         $this->view->assign('permissions', $this->permissions);
+        $this->view->assign('pageUid', $this->getPageUid());
     }
 }
