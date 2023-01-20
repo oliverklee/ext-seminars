970ff0413ea4217a863d9cea8c8b55a5b544c5f7
emilkneu <122222511+emilkneu@users.noreply.github.com>
2023-01-20 14:06:31 +0100

    [TASK] Move part of `EventDateTest` to the new TF (#2390)

--- Tests/LegacyUnit/Model/EventDateTest.php
+++ Tests/LegacyUnit/Model/EventDateTest.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace OliverKlee\Seminars\Tests\LegacyUnit\Model;
 
-use OliverKlee\Oelib\DataStructures\Collection;
 use OliverKlee\Oelib\Mapper\MapperRegistry;
 use OliverKlee\Seminars\Domain\Model\Event\EventInterface;
 use OliverKlee\Seminars\Mapper\EventMapper;
 use OliverKlee\Seminars\Model\Event;
-use OliverKlee\Seminars\Model\PaymentMethod;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -27,6 +25,7 @@ final class EventDateTest extends TestCase
 
     protected function setUp(): void
     {
+        parent::setUp();
         $this->subject = new Event();
     }
 
@@ -1260,74 +1259,5 @@ final class EventDateTest extends TestCase
         self::assertTrue(
             $this->subject->hasNotes()
         );
-    }
-
-    // Tests concerning the payment methods
-
-    /**
-     * @test
-     */
-    public function getPaymentMethodsReturnsPaymentMethodsFromTopic(): void
-    {
-        /** @var Collection<PaymentMethod> $paymentMethods */
-        $paymentMethods = new Collection();
-        $topic = new Event();
-        $topic->setData(['payment_methods' => $paymentMethods]);
-        $this->subject->setData(
-            [
-                'object_type' => EventInterface::TYPE_EVENT_DATE,
-                'topic' => $topic,
-            ]
-        );
-
-        self::assertSame(
-            $paymentMethods,
-            $this->subject->getPaymentMethods()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function setPaymentMethodsThrowsException(): void
-    {
-        $this->expectException(
-            \BadMethodCallException::class
-        );
-        $this->expectExceptionMessage(
-            'setPaymentMethods may only be called on single events and event ' .
-            'topics, but not on event dates.'
-        );
-
-        $topic = new Event();
-        $this->subject->setData(
-            [
-                'object_type' => EventInterface::TYPE_EVENT_DATE,
-                'topic' => $topic,
-            ]
-        );
-
-        /** @var Collection<PaymentMethod> $paymentMethods */
-        $paymentMethods = new Collection();
-        $this->subject->setPaymentMethods($paymentMethods);
-    }
-
-    // Tests concerning "price on request"
-
-    /**
-     * @test
-     */
-    public function getPriceOnRequestReturnsPriceOnRequestFromDopic(): void
-    {
-        $topic = new Event();
-        $topic->setData(['price_on_request' => true]);
-        $this->subject->setData(
-            [
-                'object_type' => EventInterface::TYPE_EVENT_DATE,
-                'topic' => $topic,
-            ]
-        );
-
-        self::assertTrue($this->subject->getPriceOnRequest());
     }
 }
