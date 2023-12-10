c68d6c6d30dc60cc5745b0fcdd65b2db51c10f40
Oliver Klee <typo3-coding@oliverklee.de>
2023-12-10 11:12:31 +0100

    [TASK] Move a fixture interface to its proper location (#2759)

--- Tests/LegacyUnit/Email/Fixtures/EmailSalutationHookInterface.php
+++ Tests/LegacyUnit/Email/Fixtures/EmailSalutationHookInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace OliverKlee\Seminars\Tests\LegacyUnit\Service\Fixtures;
+namespace OliverKlee\Seminars\Tests\LegacyUnit\Email\Fixtures;
 
 use OliverKlee\Seminars\Model\FrontEndUser;
 
