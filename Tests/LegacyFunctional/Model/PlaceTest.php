ef6e9e8bb1870fc6d80be560274e10aaf1779dd2
Oliver Klee <typo3-coding@oliverklee.de>
2023-12-10 17:40:39 +0100

    [TASK] Migrate `PlaceTest` (#2776)

--- Tests/LegacyFunctional/Model/PlaceTest.php
+++ Tests/LegacyFunctional/Model/PlaceTest.php
@@ -2,19 +2,28 @@
 
 declare(strict_types=1);
 
-namespace OliverKlee\Seminars\Tests\LegacyUnit\Model;
+namespace OliverKlee\Seminars\Tests\LegacyFunctional\Model;
 
 use OliverKlee\Oelib\Mapper\CountryMapper;
 use OliverKlee\Oelib\Mapper\MapperRegistry;
 use OliverKlee\Oelib\Model\Country;
 use OliverKlee\Seminars\Model\Place;
-use PHPUnit\Framework\TestCase;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
  * @covers \OliverKlee\Seminars\Model\Place
  */
-final class PlaceTest extends TestCase
+final class PlaceTest extends FunctionalTestCase
 {
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/static_info_tables',
+        'typo3conf/ext/feuserextrafields',
+        'typo3conf/ext/oelib',
+        'typo3conf/ext/seminars',
+    ];
+
     /**
      * @var Place
      */
@@ -23,6 +32,24 @@ final class PlaceTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        $countriesConnection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('static_countries');
+        if ($countriesConnection->count('*', 'static_countries', []) === 0) {
+            $countriesConnection->insert(
+                'static_countries',
+                [
+                    'uid' => 54,
+                    'cn_iso_2' => 'DE',
+                    'cn_iso_3' => 'DEU',
+                    'cn_iso_nr' => 276,
+                    'cn_official_name_local' => 'Bundesrepublik Deutschland',
+                    'cn_official_name_en' => 'Federal Republic of Germany',
+                    'cn_short_local' => 'Deutschland',
+                    'cn_short_en' => 'Germany',
+                ]
+            );
+        }
 
         $this->subject = new Place();
     }
