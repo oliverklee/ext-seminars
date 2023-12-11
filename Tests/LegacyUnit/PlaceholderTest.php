b6c151b80ba7fb29d7135b95df6100e030722671
Oliver Klee <typo3-coding@oliverklee.de>
2023-12-11 13:47:31 +0100

    [TASK] Add placeholder legacy unit test
    This will allow us to migrate the real legacy unit tests without
    having to remove the infrastructure in the same step.

---
+++ Tests/LegacyUnit/PlaceholderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Seminars\Tests\LegacyUnit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+final class PlaceholderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function theUniverseWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
