e579e0f9b30c23d2b32563ffa4e08e18b63023dd
Oliver Klee <typo3-coding@oliverklee.de>
2024-08-23 12:04:54 +0200

    [TASK] Add missing `@covers` annotations

--- Tests/Functional/FrontEnd/CategoryListTest.php
+++ Tests/Functional/FrontEnd/CategoryListTest.php
@@ -14,7 +14,9 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
+ * @covers \OliverKlee\Seminars\FrontEnd\AbstractView
  * @covers \OliverKlee\Seminars\FrontEnd\CategoryList
+ * @covers \OliverKlee\Seminars\Templating\TemplateHelper
  */
 final class CategoryListTest extends FunctionalTestCase
 {
--- Tests/LegacyFunctional/FrontEnd/RegistrationsListTest.php
+++ Tests/LegacyFunctional/FrontEnd/RegistrationsListTest.php
@@ -18,6 +18,7 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
+ * @covers \OliverKlee\Seminars\FrontEnd\AbstractView
  * @covers \OliverKlee\Seminars\FrontEnd\RegistrationsList
  * @covers \OliverKlee\Seminars\Templating\TemplateHelper
  */
