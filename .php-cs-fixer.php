bd3f4a170b9736d2b449b2776dd07b35ff87090d
Oliver Klee <typo3-coding@oliverklee.de>
2024-09-19 11:33:00 +0200

    [TASK] Use the parallel runner for PHP-CS-Fixer (#3033)
    Fixes #3030

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,6 @@
 <?php
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
+$config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
 $config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
 return $config;
