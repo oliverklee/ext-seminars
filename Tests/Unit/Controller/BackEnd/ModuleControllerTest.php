6075bc0cbd00fafe7f6fd543400cee6674e91134
Oliver Klee <typo3-coding@oliverklee.de>
2024-10-03 14:31:07 +0200

    [TASK] Make the BE controller tests a bit less specific

--- Tests/Unit/Controller/BackEnd/ModuleControllerTest.php
+++ Tests/Unit/Controller/BackEnd/ModuleControllerTest.php
@@ -91,11 +91,10 @@ final class ModuleControllerTest extends UnitTestCase
         $request = $this->createStub(ServerRequest::class);
         $this->subject->_set('request', $request);
         $this->moduleTemplateMock = $this->createMock(ModuleTemplate::class);
-        $this->moduleTemplateMock->method('renderContent')->willReturn('rendered content');
         $this->moduleTemplateFactoryMock->method('create')->with($request)->willReturn($this->moduleTemplateMock);
 
         $responseStub = $this->createStub(HtmlResponse::class);
-        $this->subject->method('htmlResponse')->with('rendered content')->willReturn($responseStub);
+        $this->subject->method('htmlResponse')->willReturn($responseStub);
 
         $this->viewMock = $this->createMock(TemplateView::class);
         $this->viewMock->method('render')->willReturn('rendered view');
