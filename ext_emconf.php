9bc80dff2ed8ba11e555f8ad9633016c38c161a7
Oliver Klee <typo3-coding@oliverklee.de>
2024-12-27 18:01:46 +0100

    [TASK] Require feuserextrafields >= 6.4.0
    Fixes #2669

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
             'php' => '7.4.0-8.3.99',
             'typo3' => '11.5.40-11.5.99',
             'extbase' => '11.5.40-11.5.99',
-            'feuserextrafields' => '6.3.0-6.99.99',
+            'feuserextrafields' => '6.4.0-6.99.99',
             'oelib' => '6.1.0-6.99.99',
             'static_info_tables' => '11.5.5-12.4.99',
         ],
