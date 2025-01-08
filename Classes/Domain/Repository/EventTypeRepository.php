3feefadd380a9da0b62856d62c91f29af98c7973
Oliver Klee <typo3-coding@oliverklee.de>
2025-01-08 23:15:36 +0100

    [TASK] Stop using the deprecated `StoragePageAgnostic` trait
    Fixes #4008

--- Classes/Domain/Repository/EventTypeRepository.php
+++ Classes/Domain/Repository/EventTypeRepository.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace OliverKlee\Seminars\Domain\Repository;
 
-use OliverKlee\Oelib\Domain\Repository\Traits\StoragePageAgnostic;
 use OliverKlee\Seminars\Domain\Model\EventType;
 use OliverKlee\Seminars\Domain\Model\EventTypeInterface;
 use OliverKlee\Seminars\Domain\Model\NullEventType;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -16,9 +17,14 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  */
 class EventTypeRepository extends Repository
 {
-    use StoragePageAgnostic;
-
     protected $defaultOrderings = ['title' => QueryInterface::ORDER_ASCENDING];
+
+    public function initializeObject(): void
+    {
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
 
     /**
      * Returns a `NullEventType` and all event types after that.
--- Classes/Domain/Repository/SpeakerRepository.php
+++ Classes/Domain/Repository/SpeakerRepository.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace OliverKlee\Seminars\Domain\Repository;
 
-use OliverKlee\Oelib\Domain\Repository\Traits\StoragePageAgnostic;
 use OliverKlee\Seminars\Domain\Model\Speaker;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -14,7 +15,12 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  */
 class SpeakerRepository extends Repository
 {
-    use StoragePageAgnostic;
-
     protected $defaultOrderings = ['name' => QueryInterface::ORDER_ASCENDING];
+
+    public function initializeObject(): void
+    {
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
 }
