caa73b773bd4bb4f039b4365adffba7ab47e7c7e
Oliver Klee <typo3-coding@oliverklee.de>
2025-01-16 11:50:49 +0100

    [TASK] Improve readability of php-cs-fixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,11 +5,11 @@ use TYPO3\CodingStandards\CsFixerConfig;
 
 $config = CsFixerConfig::create();
 
-// This is required as long as we are on PHPUnit 9.x. It can be removed after the switch to PHPUnit 10.x.
-// @see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/8337
-$rules = $config->getRules();
-$rules['php_unit_test_case_static_method_calls'] = ['call_type' => 'self', 'methods' => ['createStub' => 'this']];
-$config->setRules($rules);
+$config->addRules([
+    // This is required as long as we are on PHPUnit 9.x. It can be removed after the switch to PHPUnit 10.x.
+    // @see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/8337
+    'php_unit_test_case_static_method_calls' => ['call_type' => 'self', 'methods' => ['createStub' => 'this']],
+]);
 
 // @TODO 4.0 no need to call this manually
 $config->setParallelConfig(ParallelConfigFactory::detect());
