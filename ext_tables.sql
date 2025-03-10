2166ae8bda3b7c60bee3d9d1d39c9358a64308f9
Oliver Klee <typo3-coding@oliverklee.de>
2025-03-10 18:42:35 +0100

    [FEAUTURE] Add a field for the attachment download start date

--- ext_tables.sql
+++ ext_tables.sql
@@ -179,6 +179,7 @@ CREATE TABLE tx_seminars_seminars (
     deadline_registration int(11) unsigned DEFAULT '0' NOT NULL,
     deadline_early_bird int(11) unsigned DEFAULT '0' NOT NULL,
     deadline_unregistration int(11) unsigned DEFAULT '0' NOT NULL,
+    download_start_date int(11) unsigned DEFAULT '0' NOT NULL,
     expiry int(11) unsigned DEFAULT '0' NOT NULL,
     details_page tinytext,
     place int(11) unsigned DEFAULT '0' NOT NULL,
