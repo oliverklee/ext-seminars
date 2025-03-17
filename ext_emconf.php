258857fbedd048ac6088c21439cf2152aa7a8f01
Oliver Klee <typo3-coding@oliverklee.de>
2025-03-17 18:30:11 +0100

    [TASK] Bump the version number to 6.0.x-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Seminar Manager',
     'description' => 'Allows you to create and manage a list of seminars, workshops, lectures, theater performances and other events, allowing front-end users to sign up. FE users also can create and edit events.',
-    'version' => '5.7.0',
+    'version' => '6.0.x-dev',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
