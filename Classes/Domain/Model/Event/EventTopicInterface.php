9e78a2c0e2840b8953a8c7e4522cce8058910063
Oliver Klee <typo3-coding@oliverklee.de>
2025-05-14 17:02:43 +0200

    [FEATURE] Add `Event.categories`

--- Classes/Domain/Model/Event/EventTopicInterface.php
+++ Classes/Domain/Model/Event/EventTopicInterface.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace OliverKlee\Seminars\Domain\Model\Event;
 
+use OliverKlee\Seminars\Domain\Model\Category;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
 /**
  * This interface is required for events that contain topic information: `SingleEvent` and `EventTopic`.
  */
 interface EventTopicInterface
 {
+    /**
+     * @return ObjectStorage<Category>
+     */
+    public function getCategories(): ObjectStorage;
 }
