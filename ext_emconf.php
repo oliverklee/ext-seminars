417c0aca11b307f6e4db0c5d36405ae2412e3f40
Oliver Klee <typo3-coding@oliverklee.de>
2025-12-20 18:53:05 +0100

    V5.9.0: BE module usability improvements

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Seminar Manager',
     'description' => 'Allows you to create and manage a list of seminars, workshops, lectures, theater performances and other events, allowing front-end users to sign up. FE users also can create and edit events.',
-    'version' => '5.8.0',
+    'version' => '5.9.0',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
